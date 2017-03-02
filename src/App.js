ff84f9162347217d67abecda76c1e548faef2a2a
Tanja Hennis <tmhennis@gmail.com>
2017-03-02 18:16:04 +0100

    refactor old syntax

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 // Material UI Components
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import muiTheme from './assets/styles/theme'
 
-class App extends React.Component {
+class App extends Component {
   static childContextTypes = {
     muiTheme: React.PropTypes.object.isRequired,
   }
