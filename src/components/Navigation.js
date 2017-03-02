e3276bbc1210a112309d6ef52243cdd4bd8b2ae9
raptorzhezuzninya <w22243vrh@gmail.com>
2017-03-02 19:36:17 +0100

    create components folder for reusable components

---
+++ src/components/Navigation.js
@@ -0,0 +1,18 @@
+import React, { PureComponent, PropTypes } from 'react'
+import Appbar from 'material-ui/AppBar'
+import logo from '../assets/images/logo/logo-white.png'
+export class Navigation extends PureComponent {
+  static propTypes = {
+
+  }
+
+  render(){
+    return (
+      <Appbar
+      />
+    )
+  }
+}
+
+export default Navigation
+
