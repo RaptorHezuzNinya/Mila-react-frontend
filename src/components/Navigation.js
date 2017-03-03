a0c92dc9a9acf6d1fa4903b5934182c515ddd3dc
raptorzhezuzninya <w22243vrh@gmail.com>
2017-03-03 12:35:32 +0100

    add needed buttons to navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,16 +1,34 @@
 import React, { PureComponent, PropTypes } from 'react'
+
 import Appbar from 'material-ui/AppBar'
-import logo from '../assets/images/logo/logo-white.png'
+import Flatbutton from 'material-ui/FlatButton'
+
+import './Navigation.sass'
+import milaLogo from '../assets/images/logo/logo-white.png'
+
 export class Navigation extends PureComponent {
   static propTypes = {
-
   }
 
   render(){
+    const navButtonStyle = {
+      color: 'white'
+    }
+
+    const rightLinks = (
+      <div>
+        <Flatbutton label="New Contacts" style={navButtonStyle} />
+        <Flatbutton label="My network" style={navButtonStyle} />
+      </div>
+    )
+
     return (
       <Appbar
+        iconElementLeft={<img className="logo-mila" src={milaLogo}  />}
+        iconElementRight={ rightLinks }
       />
     )
+
   }
 }
 
