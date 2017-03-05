f2c8ffb1a2d13c296cc76fe73f936e55e52802c5
raptorzhezuzninya <w22243vrh@gmail.com>
2017-03-05 23:55:28 +0100

    add const to render multiple logos right

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,16 @@
 import React, { PureComponent, PropTypes } from 'react'
+import { Link } from 'react-router'
 
 // material-ui Components
 import Appbar from 'material-ui/AppBar'
 import Flatbutton from 'material-ui/FlatButton'
-
+import IconButton from 'material-ui/IconButton'
 // logos icons
-import milaLogo from '../assets/images/logo/logo-white.png'
+import UserIconBlue from '../assets/images/icons/user-blue.jpg'
+import UserIconGrey from '../assets/images/icons/user-grey.svg'
+import milaLogo from '../assets/images/logos/logo-white.png'
+import ArrowDown from 'material-ui/svg-icons/hardware/keyboard-arrow-down'
+import UserCircle from 'material-ui/svg-icons/action/account-circle'
 
 // Styles
 import './Navigation.sass'
