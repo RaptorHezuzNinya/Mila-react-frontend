afa7663ef169fe421ee4fa01dd2d76c48b1e604f
Tanja Hennis <tmhennis@gmail.com>
2017-03-06 11:22:11 +0100

    contactcard first tests

---
+++ src/newContacts/ContactCard.test.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import chai, { expect } from 'chai'
+
+import chaiEnzyme from 'chai-enzyme'
+chai.use(chaiEnzyme())
+
+import wrapper from '~/../test/wrapper'
+import ContactCard from './ContactCard'
+
+describe('<ContactCard />', () => {
+  const contactcard = wrapper(<ContactCard />)
+
+  it('is a card', () => {
+    expect(contactcard).to.have.tagName('div')
+    expect(contactcard).to.have.className('card')
+  })
+})
