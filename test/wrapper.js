e45d14d5de6d4221abce6ab8cdae180c8d38cbc1
Tanja Hennis <tmhennis@gmail.com>
2017-03-06 11:19:21 +0100

    added muitheme to test wrapper

--- test/wrapper.js
+++ test/wrapper.js
@@ -3,10 +3,15 @@ import { Provider } from 'react-redux'
 import { shallow, mount, render } from 'enzyme'
 import store from '~/store'
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import muiTheme from '~/assets/styles/theme'
+
 export default (node, nodeContext = {}) => {
   let context = Object.assign({}, nodeContext, { store })
   return mount(
     <Provider store={store}>
-      {node}
+      <MuiThemeProvider muiTheme={muiTheme}>
+        {node}        
+      </MuiThemeProvider>
     </Provider>, { context })
 }
