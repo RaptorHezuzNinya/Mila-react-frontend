bca51df42192f8dbed30dfb513c7e6dfedd78f42
raptorzhezuzninya <w22243vrh@gmail.com>
2017-03-06 20:08:18 +0100

    cleaned up import in navi.test

--- src/components/Navigation.test.js
+++ src/components/Navigation.test.js
@@ -1,13 +1,7 @@
 import React from 'react'
 import chai, { expect } from 'chai'
-
-import chaiEnzyme from 'chai-enzyme'
-chai.use(chaiEnzyme())
-
-import spies from 'chai-spies'
-chai.use(spies)
-
 import wrapper from '../../test/helpers/wrapper'
+
 import { Navigation } from './Navigation'
 
 describe('<Navigation/>', () => {
