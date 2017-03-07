dd117c68bea5a4c973a945aaee97c52e22efcaad
Tanja Hennis <tmhennis@gmail.com>
2017-03-07 17:48:45 +0100

    Contact test renamed

---
+++ src/newContacts/Contact.test.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import chai, { expect } from 'chai'
+
+import chaiEnzyme from 'chai-enzyme'
+chai.use(chaiEnzyme())
+
+import wrapper from '~/../test/helpers/wrapper'
+import Contact from './Contact'
+
+describe('<Contact />', () => {
+  const contact = wrapper(<Contact />)
+
+  it('is a card', () => {
+    expect(contact).to.have.tagName('div')
+    expect(contact).to.have.className('card')
+  })
+})
