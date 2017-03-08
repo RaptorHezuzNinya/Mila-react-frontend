d27ce1e88c433be9903eedc28e3f745bbcba4156
raptorzhezuzninya <w22243vrh@gmail.com>
2017-03-08 16:59:05 +0100

    add comment

--- src/newContacts/NewContactsContainer.js
+++ src/newContacts/NewContactsContainer.js
@@ -1,6 +1,7 @@
 import React, { PureComponent, PropTypes } from 'react'
 import { connect } from 'react-redux'
 
+// Components
 import Contact from './Contact'
 
 // exporting it here seems redundant but is necessary for shallow rendering in tests.
