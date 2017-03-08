664d37fb0e6636d8eb32ef5f8dedb42527215843
raptorzhezuzninya <w22243vrh@gmail.com>
2017-03-08 14:09:13 +0100

    connected network container with store

--- src/myNetwork/NetworkContainer.js
+++ src/myNetwork/NetworkContainer.js
@@ -1,11 +1,22 @@
 import React, { PureComponent, PropTypes } from 'react'
+import { connect } from 'react-redux'
+
+// Components
+import Network from './Network'
 
 class NetworkContainer extends PureComponent {
+  static propTypes = {
+
+  }
+
   render(){
     return (
-      <h1>Yoloswag</h1>
+      <div>
+        <h1> NetworkContainer </h1>
+        <Network content="Network comp" />
+      </div>
     )
   }
 }
 
-export default NetworkContainer
+export default connect(null)(NetworkContainer)
