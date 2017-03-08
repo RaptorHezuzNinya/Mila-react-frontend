0997919820d1e75208992b416138f1c899a541ed
raptorzhezuzninya <w22243vrh@gmail.com>
2017-03-08 13:16:32 +0100

    setup dumb skeleton containerNetwork

---
+++ src/myNetwork/NetworkContainer.js
@@ -0,0 +1,11 @@
+import React, { PureComponent, PropTypes } from 'react'
+
+class NetworkContainer extends PureComponent {
+  render(){
+    return (
+      <h1>Yoloswag</h1>
+    )
+  }
+}
+
+export default NetworkContainer
