4ca758dd7f34423b4877b9036c55453b4c9b9f49
raptorzhezuzninya <w22243vrh@gmail.com>
2017-03-08 16:29:42 +0100

    create new networklist reducer

---
+++ src/reducers/networklists.js
@@ -0,0 +1,22 @@
+const initialState = [
+  {
+    listId: 1,
+    name: 'Work',
+    avatar: '~/assets/images/avatars/troll.png'
+  },
+  {
+    listId: 2,
+    name: 'Devops',
+    avatar: '~/assets/images/avatars/troll.png'
+  },
+  {
+    listId: 3,
+    name: 'Business Int',
+    avatar: '~/assets/images/avatars/troll.png'
+  },
+  {
+    listId: 4,
+    name: 'k',
+    avatar: '~/assets/images/avatars/troll.png'
+  },
+]
