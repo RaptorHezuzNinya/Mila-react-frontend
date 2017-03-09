3821382e6307e46845b5de7b7540035baf5dbea4
raptorzhezuzninya <w22243vrh@gmail.com>
2017-03-09 10:57:09 +0100

    add <DataTables /> to networklist

--- src/myNetwork/NetworkList.js
+++ src/myNetwork/NetworkList.js
@@ -1,16 +1,22 @@
 import React, { PureComponent, PropTypes } from 'react'
 
+// Actions && Reducers
+import networkList from '~/reducers/networklists'
+
+// Material UI Components
+import DataTables from 'material-ui-datatables'
+
 class NetworkList extends PureComponent {
+
   static propTypes = {
 
   }
 
   render(){
     const { name, avatar, listId } = this.props
-
     return (
       <div className="networklist">
-        <h3>{name}</h3>
+        <DataTables />
       </div>
 
     )
