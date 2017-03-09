2cdae66d1e7a26d25cca312cdaa1371386e64040
raptorzhezuzninya <w22243vrh@gmail.com>
2017-03-09 15:57:13 +0100

    add renderNetworkbutton to network

--- src/myNetwork/Network.js
+++ src/myNetwork/Network.js
@@ -1,26 +1,33 @@
 import React, { PureComponent, PropTypes } from 'react'
 
 // Components
+import NetworkButton from '../Components/NetworkButton'
 import NetworkList from './NetworkList'
 
 class Network extends PureComponent {
+
   static propTypes = {
     networkLists: PropTypes.array.isRequired
   }
 
-  renderNetworkList(networklist, index) {
-    return <NetworkList key={ index } { ...networklist } />
+  renderNetworkButton(networkList, index){
+    return <NetworkButton key={ index } { ...networkList }/>
   }
 
   render() {
-    
+
     const { networkLists } = this.props
     if (!networkLists) return null
 
     return (
       <div className="network-component">
-        { networkLists.map(this.renderNetworkList.bind(this))}
+        <div className="networkbutton">
+          { networkLists.map(this.renderNetworkButton.bind(this)) }
+        </div>
+        <div className="networklist">
+        </div>
       </div>
+
     )
   }
 }
