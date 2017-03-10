ce967e9400c99323139555b114b95189c2e96587
raptorzhezuzninya <w22243vrh@gmail.com>
2017-03-10 18:05:09 +0100

    add createnetworklist component

--- src/components/CreateNetworkButton.js
+++ src/components/CreateNetworkButton.js
@@ -1,15 +1,52 @@
 import React, { PureComponent, PropTypes } from 'react'
+import { connect } from 'react-redux'
 
-// Actions & reduercs
+// Actions & Reducers
+import networkList from '../reducers/networklists'
+import createNetworkList from '../actions/networklists/create'
 
+//material-ui Components
+import FlatButton from 'material-ui/FlatButton'
+import TextField from 'material-ui/TextField'
 
 class CreateNetworkButton extends PureComponent {
+  state = {
+    name: this.props.name
+  }
+
+  createNetworkList(event){
+    event.preventDefault()
+    const { name } = this.state
+
+    this.props.createNetworkList(this.state)
+  }
+
+  handleChange = (event) => {
+    const field = event.target.name
+    this.setState({
+      [field]: event.target.value
+    })
+  }
 
   render(){
-    return {
-
-    }
+    const { name } = this.props
+    return (
+      <div>
+        <TextField
+          type="text"
+          ref="listtitle"
+          name="name"
+          value={this.state.name || ''}
+          onChange={this.handleChange}
+        />
+        <FlatButton
+          className="unicorn"
+          label="+"
+          onClick={this.createNetworkList.bind(this)}
+        />
+      </div>
+    )
   }
 }
 
-export default CreateNetworkButton
+export default connect(null, { createNetworkList })(CreateNetworkButton)
