81579eac4ebeb8fda056f7a5d0c202bf0bc8469e
raptorzhezuzninya <w22243vrh@gmail.com>
2017-03-10 17:20:01 +0100

    add new createNetworkbutton

---
+++ src/components/CreateNetworkButton.js
@@ -0,0 +1,15 @@
+import React, { PureComponent, PropTypes } from 'react'
+
+// Actions & reduercs
+
+
+class CreateNetworkButton extends PureComponent {
+
+  render(){
+    return {
+
+    }
+  }
+}
+
+export default CreateNetworkButton
