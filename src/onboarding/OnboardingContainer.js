ad022ffe1d0091d739c8abbb72d8093322e39648
nienkebos <nienkebos@live.com>
2017-03-13 11:57:08 +0100

    Setup OnboardingContainer and route

---
+++ src/onboarding/OnboardingContainer.js
@@ -0,0 +1,20 @@
+import React, { PureComponent, PropTypes } from 'react'
+import { connect } from 'react-redux'
+
+// Components
+// import Onboarding from './Onboarding'
+
+class OnboardingContainer extends PureComponent {
+
+  render() {
+    return (
+      <div>
+        <h1>OnboardingContainer :)</h1>
+
+      </div>
+    )
+  }
+
+}
+
+export default OnboardingContainer
