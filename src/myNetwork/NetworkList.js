fd84e5852ff83aeaf39887180916c7a79ef9412c
raptorzhezuzninya <w22243vrh@gmail.com>
2017-03-15 23:35:04 +0100

    add 4th const for 4th colum

--- src/myNetwork/NetworkList.js
+++ src/myNetwork/NetworkList.js
@@ -71,6 +71,7 @@ class NetworkList extends PureComponent {
     const thNameEmail = document.getElementsByClassName('th-name-email')
     const thCompany = document.getElementsByClassName('th-company')
     const thEmails = document.getElementsByClassName('th-emails')
+    const thLast = document.getElementsByClassName('th-last')
     if (width < 480) {
       thNameEmail[0].setAttribute('colSpan', '12')
     } else if (width >= 480) {
