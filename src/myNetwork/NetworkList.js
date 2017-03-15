909cc8c37cddad1aff8774ec9b1e0acad3b01994
raptorzhezuzninya <w22243vrh@gmail.com>
2017-03-15 18:55:08 +0100

    add colspan for testz

--- src/myNetwork/NetworkList.js
+++ src/myNetwork/NetworkList.js
@@ -142,18 +142,18 @@ class NetworkList extends PureComponent {
           >
 
             {contacts.map( (contact, index) => (
-              <TableRow style={styles.tableRow} key={index} selected={contact.selected}>
+              <TableRow style={styles.tableRow} key={index} selected={contact.selected} colSpan="12">
                 <TableRowColumn className="col-avatar">
                   <Avatar src={contact.avatar}
                           style={styles.avatar}
                   />
                 </TableRowColumn>
-                <TableRowColumn className="col-name-email">
+                <TableRowColumn className="col-name-email" colSpan="8">
                   <p className="name-email">
                     { contact.firstName + ' ' + contact.lastName } <br/> { contact.email }
                   </p>
                 </TableRowColumn>
-                <TableRowColumn className="col-company">
+                <TableRowColumn className="col-company" colSpan="4">
                   <p className="company-name">
                     { contact.companyName }
                   </p>
