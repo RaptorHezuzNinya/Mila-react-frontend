2e04898fd0d928391e01a968c3d60facda6b06ae
raptorzhezuzninya <w22243vrh@gmail.com>
2017-03-15 22:45:55 +0100

    fiddling to expect reSize function

--- src/myNetwork/NetworkList.js
+++ src/myNetwork/NetworkList.js
@@ -75,13 +75,20 @@ class NetworkList extends PureComponent {
     } else if (width >= 480) {
       const thNameEmail = document.getElementsByClassName('th-name-email')
       const thCompany = document.getElementsByClassName('th-company')
-
       thNameEmail[0].setAttribute('colSpan', '8')
       thCompany[0].setAttribute('colSpan', '4')
     }
+    // else if (width > 769) {
+    //   console.log('lol')
+    //   const thNameEmail = document.getElementsByClassName('th-name-email')
+    //   const thCompany = document.getElementsByClassName('th-company')
+    //   const thEmails = document.getElementsByClassName('th-emails')
+    //   thNameEmail[0].setAttribute('colSpan', '3')
+    //   thCompany[0].setAttribute('colSpan', '3')
+    //   thEmails[0].setAttribute('colSpan', '6')
+    // }
 
   };
-
 
 
   renderContactRow(row, index) {
@@ -111,7 +118,6 @@ class NetworkList extends PureComponent {
           selectable={this.state.selectable}
           multiSelectable={this.state.multiSelectable}
         >
-
 
           <TableHeader
             className="table-head"
