753715f5a587b31a1a07f8bc9d83c1a554457e93
Tanja Hennis <tmhennis@gmail.com>
2017-03-16 16:44:49 +0100

    initial creation of contactslider

---
+++ src/newContacts/ContactSlider.js
@@ -0,0 +1,15 @@
+import React, { PureComponent, PropTypes} from 'react'
+import { connect } from 'react-redux'
+
+// Components
+import Contact from './Contact'
+
+class ContactSlider extends PureComponent {
+  render() {
+    return (
+      <div>I'm a slider!</div>
+    )
+  }  
+}
+
+export default ContactSlider
