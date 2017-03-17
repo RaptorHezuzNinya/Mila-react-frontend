fe2ba91fcf074e3475884de406b1f3383f59535b
raptorzhezuzninya <w22243vrh@gmail.com>
2017-03-17 12:52:41 +0100

    add wrapper class and renamed first nested componenet

--- src/myNetwork/NetworkList.js
+++ src/myNetwork/NetworkList.js
@@ -132,9 +132,9 @@ class NetworkList extends PureComponent {
     const { contacts} = this.props
 
     return (
-      <div>
+      <div className="wrapper">
         <Table
-          className="table-container"
+          className="container-table"
           height={this.state.height}
           fixedHeader={this.state.fixedHeader}
           fixedFooter={this.state.fixedFooter}
