546a81e82bb419928315f92b0b55eb706dd590aa
raptorzhezuzninya <w22243vrh@gmail.com>
2017-03-17 17:53:44 +0100

    setup modal reducer

--- src/reducers/modals.js
+++ src/reducers/modals.js
@@ -2,3 +2,18 @@ const initialState = {
   modalType: null,
   payload: {}
 }
+
+export default function modals( state = initialState, action) {
+  switch (action.type) {
+    case SHOW_MODAL:
+      return {
+        modalType: action.modalType,
+        payload: action.payload,
+
+      }
+
+      break;
+    default:
+
+  }
+}
