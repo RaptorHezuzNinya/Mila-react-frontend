b3757992c0e31f03bfd086b142239e62956dacbe
raptorzhezuzninya <w22243vrh@gmail.com>
2017-03-17 13:31:46 +0100

    add material ui dialog to use as our reusable modal comp

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,7 +1,52 @@
 import React, { PropTypes, PureComponent } from 'react'
 
-class Modal extends PureComponent {
+// material ui Components
+import Dialog from 'material-ui/Dialog'
+import FlatButton from 'material-ui/FlatButton'
+import RaisedButton from 'material-ui/RaisedButton'
 
+class Modal extends PureComponent {
+  state = {
+    open: false
+  }
+
+  handleOpen = () => {
+    this.setState({open: true})
+  }
+
+  handleClose = () => {
+    this.setState({open: false})
+  }
+
+  render() {
+    const actions = [
+      <FlatButton
+        label="Cancel"
+        primary={true}
+        onTouchTap={this.handleClose}
+      />,
+      <FlatButton
+        label="Submit"
+        primary={true}
+        disabled={true}
+        onTouchTap={this.handleClose}
+      />,
+    ]
+
+    return (
+      <div>
+        <RaisedButton label="Create List" onTouchTap={this.handleOpen} />
+        <Dialog
+          title="Dialog With Actions"
+          actions={actions}
+          modal={true}
+          open={this.state.open}
+        >
+          Only actions can close this dialog.
+        </Dialog>
+      </div>
+    )
+  }
 }
 
 export default Modal
