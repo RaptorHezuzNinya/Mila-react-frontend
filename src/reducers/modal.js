e8d006357be6edbcc9fe8c5e1e267013c8c58484
raptorzhezuzninya <w22243vrh@gmail.com>
2017-03-20 12:54:01 +0100

    add hide modal

--- src/reducers/modal.js
+++ src/reducers/modal.js
@@ -1,4 +1,5 @@
 import { SHOW_MODAL } from '../actions/modals/show-modal'
+// import { HIDE_MODAL } from '../actions/modals/hide-modal'
 
 const initialState = {
   modalType: null,
@@ -12,6 +13,8 @@ export default function modal( state = initialState, action) {
         modalType: action.modalType,
         modalProps: action.modalProps,
       }
+    // case HIDE_MODAL :
+    //   return initialState
 
     default:
       return state
