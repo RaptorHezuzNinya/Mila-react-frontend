dd046027f26fe5d16b5fa128365f2c458fc8b9d0
raptorzhezuzninya <w22243vrh@gmail.com>
2017-03-20 18:10:29 +0100

    add which specifi modal type i need for the show modal action, will rename show-modal soon

--- src/actions/modals/show-modal.js
+++ src/actions/modals/show-modal.js
@@ -1,11 +1,11 @@
 export const SHOW_MODAL = 'SHOW_MODAL'
 
-export default (showModal) => {
+export default (showModal, CREATE_NETWORKLIST_MODAL) => {
 
   return {
     type: SHOW_MODAL,
     payload: {
-      modalType: {},
+      modalType: 'CREATE_NETWORKLIST_MODAL',
       modalProps: {}
     }
   }
