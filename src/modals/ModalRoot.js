e56fa29ab122e058832e6e205206f21dd90509ca
raptorzhezuzninya <w22243vrh@gmail.com>
2017-03-21 12:02:12 +0100

    refactor mapstatetoprops to store modal.state so popping a modal works

--- src/modals/ModalRoot.js
+++ src/modals/ModalRoot.js
@@ -1,22 +1,26 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import createNetworkListModal from './createNetworkListModal'
+import CreateNetworkListModal from './CreateNetworkListModal'
 
 const MODAL_COMPONENTS = {
-  'CREATE_NETWORKLIST_MODAL': createNetworkListModal,
+  'CREATE_NETWORKLIST_MODAL': CreateNetworkListModal,
   // other modals
 }
 
 const ModalRoot = ({ modalType, modalProps }) => {
+  console.log('zitten voor IF in ROOT')
+
   if (!modalType) {
     return null
   }
-  console.log('ZITTEN IN ELSE')
   const SpecificModal = MODAL_COMPONENTS[modalType]
 
   return <SpecificModal { ...modalProps } />
 }
 
-const mapStateToProps = ({ modal }) => ({ modal })
+// const mapStateToProps = ({ modal }) => ({ modal })
+//
+// export default connect(mapStateToProps)(ModalRoot)
+const mapStateToProps = state => state.modal
 
 export default connect(mapStateToProps)(ModalRoot)
