64302f762146d1287f7817d298e3ca210de0e28c
raptorzhezuzninya <w22243vrh@gmail.com>
2017-04-11 15:34:47 +0200

    add style object to overrite card default styles

--- src/onboarding/Onboarding.js
+++ src/onboarding/Onboarding.js
@@ -1,23 +1,22 @@
 import React, { PureComponent, PropTypes } from 'react'
-
 // Material UI Components
-import { Card, CardHeader, CardActions, CardText } from 'material-ui/Card'
-import TextField from 'material-ui/TextField'
-import FlatButton from 'material-ui/FlatButton'
-
+import { Card } from 'material-ui/Card'
 // Components
-
 import OnboardingStepper from './OnboardingStepper'
-
 // Styles
 import './Onboarding.sass'
 
+const styles = {
+  card: {
+    height: '100vh'
+  }
+}
 class Onboarding extends PureComponent {
 
   render() {
     return (
       <div className="onboarding-card">
-        <Card>
+        <Card style={styles.card}>
           <OnboardingStepper />
         </Card>
       </div>
