c214b4192f054f50cb8fb31f54feb2b485b9ca16
raptorzhezuzninya <w22243vrh@gmail.com>
2017-04-11 13:31:46 +0200

    refactor scanningcontact from class comp to functional comp since it holds no logic

--- src/onboarding/ScanningContacts.js
+++ src/onboarding/ScanningContacts.js
@@ -1,25 +1,15 @@
-import React, { PureComponent, PropTypes } from 'react'
-
-// Material UI Components
-import { Card, CardHeader, CardActions, CardText } from 'material-ui/Card'
-import TextField from 'material-ui/TextField'
-import FlatButton from 'material-ui/FlatButton'
-
-
+import React from 'react'
 // Styles
 import './onboarding.sass'
 
-class ScanningContacts extends PureComponent {
-
-  render() {
-    return (
-      <div className="scanning-contacts">
-        <h1>Hi User!</h1>
-        <p>I'm Mila. I need about 30-40 seconds to scan your inbox for your contacts.</p>
-        <p>In the meantime, tell me how you want to keep in touch with your network.</p>
-      </div>
-    )
-  }
+const ScanningContacts = () => {
+  return (
+    <div className="scanning-contacts">
+      <h3>{"Hi #{dynamic UserName}!"}</h3>
+      <p>I'm Mila. I need about 30-40 seconds to scan your inbox for your contacts.</p>
+      <p>In the meantime, tell me how you want to keep in touch with your network.</p>
+    </div>
+  )
 }
 
 export default ScanningContacts
