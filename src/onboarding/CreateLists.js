12a4604ef106138a9886e3c8edb10d338507bc5e
raptorzhezuzninya <w22243vrh@gmail.com>
2017-04-11 19:09:14 +0200

    removed alot of not needed stuff

--- src/onboarding/CreateLists.js
+++ src/onboarding/CreateLists.js
@@ -1,10 +1,8 @@
 import React, { PureComponent, PropTypes } from 'react'
 
 // Material UI Components
-import { Card, CardHeader, CardActions, CardText } from 'material-ui/Card'
-import TextField from 'material-ui/TextField'
 import FlatButton from 'material-ui/FlatButton'
-
+import TextField from 'material-ui/TextField'
 
 // Styles
 import './CreateLists.sass'
