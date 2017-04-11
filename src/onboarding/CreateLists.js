f52f98db44b118bba8670cd51336c3135e4b274a
raptorzhezuzninya <w22243vrh@gmail.com>
2017-04-11 22:56:30 +0200

    add inline mui style object overwrites

--- src/onboarding/CreateLists.js
+++ src/onboarding/CreateLists.js
@@ -4,9 +4,17 @@ import React, { PureComponent, PropTypes } from 'react'
 import FlatButton from 'material-ui/FlatButton'
 import TextField from 'material-ui/TextField'
 import ListIcon from 'material-ui/svg-icons/action/list'
+import {orange500, blue500} from 'material-ui/styles/colors';
 
 // Styles
 import './CreateLists.sass'
+
+const styles = {
+  hint: {
+    fontSize: 14,
+    fontFamily: 'Montserrat-Light'
+  }
+}
 
 class CreateLists extends PureComponent {
   constructor(props){
@@ -20,38 +28,34 @@ class CreateLists extends PureComponent {
     this.setState({
       listTitle: event.target.value
     })
-  }
-
-  clickDo = () => {
-    const { listTitle } = this.state.listTitle
-    this.props.saveList(listTitle)
-  }
-
-  renderLists = (list, index) => {
-    return (
-      <TextField
-        key={index}
-        defaultValue={list.name}
-      />
-    )
+    console.log(this.state.listTitle)
   }
 
   render() {
-    const { myLists } = this.state
     return (
       <div className="create-lists-wrapper">
         <p>Ok, In the mean time, tell me: Which lists shall we use to sort your contacts? Do you (want to) keep a newslettes, sales-funnel, prospects?
         </p>
-        <div className="example-list">
+        {/* <div className="example-list">
           <span><ListIcon className="list-icon"/></span>
           <p className="example-title">Stakeholders</p>
+        </div> */}
+        <div className="list-form-holder">
+          <form>
+            <TextField
+              onChange={this.handleChange}
+              className="list-input"
+              hintText="Enter list title, e.g. clients, prospects …"
+              hintStyle={styles.hint}
+              fullWidth={true}/>
+
+            <div className="form-btn-holder">
+              <FlatButton
+                className="btn-grey form-btn"
+                label="Add a List"/>
+            </div>
+          </form>
         </div>
-        <form>
-          <TextField placeholder="Enter your list title here"/>
-          <FlatButton
-            className="btn-grey"
-            label="Add a List"/>
-        </form>
       </div>
     )
   }
