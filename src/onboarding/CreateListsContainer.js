bde3939cec7fade684463efa5153feedc11ff702
raptorzhezuzninya <w22243vrh@gmail.com>
2017-04-12 18:13:01 +0200

    import field and redux form

--- src/onboarding/CreateListsContainer.js
+++ src/onboarding/CreateListsContainer.js
@@ -1,6 +1,6 @@
 import React, { PureComponent, PropTypes } from 'react'
 import { connect } from 'react-redux'
-
+import { Field, reduxForm } from 'redux-form'
 // actions
 import createNetworkList from '../actions/networklists/create'
 
