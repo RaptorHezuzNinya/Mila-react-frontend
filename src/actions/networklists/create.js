a0cb6621bc27c4a90b570b65c7f186eda102c09a
raptorzhezuzninya <w22243vrh@gmail.com>
2017-04-12 19:38:01 +0200

    refactor create action

--- src/actions/networklists/create.js
+++ src/actions/networklists/create.js
@@ -1,16 +1,20 @@
 export const CREATE_NETWORKLIST = 'CREATE_NETWORKLIST'
 
-// export default (createNetworkList) => {
-//   return {
-//     type: CREATE_NETWORKLIST,
-//     payload: createNetworkList
-//   }
-// }
-
-export function createNetworkList(networkList){
-
+export default (createNetworkList) => {
   return {
     type: CREATE_NETWORKLIST,
-    payload: networkList
+    payload: createNetworkList
   }
 }
+
+
+// If you wanna stack multiple action in 1 index.js file write them like this and....
+// import === import { createNetworkList } from '../actions/networklists/create'
+//
+// export function createNetworkList(networkList){
+//
+//   return {
+//     type: CREATE_NETWORKLIST,
+//     payload: networkList
+//   }
+// }
