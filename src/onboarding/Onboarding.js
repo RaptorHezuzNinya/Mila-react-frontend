6884d93365d5bde3ef218b4f84f3799e029e1958
raptorzhezuzninya <w22243vrh@gmail.com>
2017-04-12 14:59:14 +0200

    removed card since it only fucked shit up

--- src/onboarding/Onboarding.js
+++ src/onboarding/Onboarding.js
@@ -1,25 +1,16 @@
 import React, { PureComponent, PropTypes } from 'react'
 // Material UI Components
-import { Card } from 'material-ui/Card'
 // Components
 import OnboardingStepper from './OnboardingStepper'
 // Styles
 import './Onboarding.sass'
 
-const styles = {
-  card: {
-    height: '100vh'
-  }
-}
-
 class Onboarding extends PureComponent {
 
   render() {
     return (
-      <div className="onboarding-card">
-        <Card style={styles.card}>
-          <OnboardingStepper />
-        </Card>
+      <div>
+        <OnboardingStepper />
       </div>
     )
   }
