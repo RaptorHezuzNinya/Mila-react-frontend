ec80d61d2f5e6f3257949957b1f0fff9f0dfe8d0
raptorzhezuzninya <w22243vrh@gmail.com>
2017-04-14 15:17:13 +0200

    add startsorting comp for last step

---
+++ src/onboarding/StartSorting.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const StartSorting = () => {
+  return (
+    <div className="startsorting-holder">
+      <p>
+        Alright!
+      </p>
+      <p>
+        You can always sync your lists with any newsletter or sales tool. But let's worry about that later.
+      </p>
+      <p>
+        Lets's start sorting the 25 most interesting people I found!
+      </p>
+    </div>
+  )
+}
+
+export default StartSorting
