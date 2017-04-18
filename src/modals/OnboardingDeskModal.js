960d7ae6c3845e9ad85bf8722eb683cb7b93c71f
raptorzhezuzninya <w22243vrh@gmail.com>
2017-04-18 13:25:39 +0200

    setup connected onboardingdeskmodal

--- src/modals/OnboardingDeskModal.js
+++ src/modals/OnboardingDeskModal.js
@@ -1,7 +1,65 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
+import { connect } from 'react-redux'
+// Components
+import OnboardingStepper from '../onboarding/OnboardingStepper'
+// actions
+import { hideModal } from '../actions/modals/index'
+// material-ui Components
+import Dialog from 'material-ui/Dialog'
+import FlatButton from 'material-ui/FlatButton'
 
-const OnboardingDeskModal = () => {
-
+const customContentStyle = {
+  width: '50%',
+  height: '500px',
+  maxWidth: 'none',
 }
 
-export default OnboardingDeskModal
+class OnboardingDeskModal extends PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = {
+      open: true
+    }
+  }
+
+  handleOpen = () => {
+    this.setState({open: true})
+  }
+
+  handleClose = () => {
+    this.setState({open: false})
+    this.props.hideModal()
+  }
+
+  render(){
+    const actions = [
+      <FlatButton
+        label="Cancel"
+        primary={true}
+        onTouchTap={this.handleClose}
+      />,
+      <FlatButton
+        label="Submit"
+        primary={true}
+        disabled={true}
+        onTouchTap={this.handleClose}
+      />,
+    ]
+
+    return (
+      <div>
+        <Dialog
+          title="dialog title"
+          actions={actions}
+          modal={true}
+          contentStyle={customContentStyle}
+          open={this.state.open}
+        >
+          <OnboardingStepper />
+        </Dialog>
+      </div>
+    )
+  }
+}
+
+export default connect(null, {hideModal})(OnboardingDeskModal)
