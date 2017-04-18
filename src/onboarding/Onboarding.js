daf8976e4c23d60fbedddd90dc039528ca1f7bf2
raptorzhezuzninya <w22243vrh@gmail.com>
2017-04-18 20:59:46 +0200

    cuz of react-media omfg LOL

--- src/onboarding/Onboarding.js
+++ src/onboarding/Onboarding.js
@@ -1,65 +1,25 @@
 import React, { PureComponent, PropTypes } from 'react'
+import Media from 'react-media'
 // Components
 import OnboardingDeskModal from '../modals/OnboardingDeskModal'
 import OnboardingStepper from './OnboardingStepper'
-import ButtonModal from '../modals/ButtonModal'
-import ModalRoot from '../modals/ModalRoot'
 // Styles
 import './Onboarding.sass'
 
 class Onboarding extends PureComponent {
-  constructor(props){
-    super(props)
-    this.state = {
-      mobileView: false
-    }
-    this.onResize = this.onResize.bind(this)
-  }
-
-  componentDidMount () {
-    this.onResize()
-    window.addEventListener('resize', this.onResize)
-  }
-
-  componentWillUnmount () {
-    window.removeEventListener('resize', this.onResize)
-  }
-
-  onResize () {
-    const width = window.innerWidth
-    if (width <= 769) {
-      console.log('in &&&&')
-      this.setState({
-        mobileView: true,
-      })
-    } else {
-      this.setState({
-        mobileView: false,
-      })
-    }
-  }
-
   render() {
-    const { mobileView } = this.state
-
-    let onboardingStepper
-    if (mobileView) {
-      console.log('WHEN DO I GET CALLED XX')
-      return <OnboardingStepper />
-    } else {
-      console.log('WHEN DO I GET CALLED YY')
-      return <OnboardingDeskModal />
-    }
 
     return (
       <div>
-        { OnboardingStepper }
+        <Media query="(max-width: 769px)" render={() => (
+          <OnboardingStepper />
+        )}/>
+        <Media query="(min-width: 769px)" render={() => (
+          <OnboardingDeskModal />
+        )}/>
       </div>
     )
   }
 }
 
 export default Onboarding
-
-// {/* <ButtonModal usedStyle='' label='Start onboard desk' modal={ onboardingDeskModal } /> */}
-// () => this.props.dispatch(this.props.showModal(this.state.onboardingDeskModal))
