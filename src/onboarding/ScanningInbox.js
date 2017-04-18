f658b65dbe1e4f92cd517da8578e900751872aa9
raptorzhezuzninya <w22243vrh@gmail.com>
2017-04-19 00:56:18 +0200

    removed stylesheet import sice it gets acces to sheets from parent

--- src/onboarding/ScanningInbox.js
+++ src/onboarding/ScanningInbox.js
@@ -1,10 +1,8 @@
 import React from 'react'
-// Styles
-import './Onboarding.sass'
 
 const ScanningInbox = () => {
   return (
-    <div className="scanning-contacts">
+    <div className="scanning-inbox">
       <h3>{"Hi #{username}!"}</h3>
       <p>I am Mila. Nice to meet you.</p>
       <p>I’ve already started scanning your inbox in the background. From now on, it’s my job to keep track of all the valuable contacts in your inbox.
