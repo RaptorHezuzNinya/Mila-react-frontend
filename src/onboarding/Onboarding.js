75cd69774eff38cd7b115b7661858cd7516359d1
raptorzhezuzninya <w22243vrh@gmail.com>
2017-04-18 13:27:26 +0200

    ad dmodal button to onboarding to open onboardingmodal

--- src/onboarding/Onboarding.js
+++ src/onboarding/Onboarding.js
@@ -1,16 +1,32 @@
 import React, { PureComponent, PropTypes } from 'react'
-// Material UI Components
 // Components
 import OnboardingStepper from './OnboardingStepper'
+import ButtonModal from '../modals/ButtonModal'
+import ModalRoot from '../modals/ModalRoot'
 // Styles
 import './Onboarding.sass'
 
 class Onboarding extends PureComponent {
+  constructor(props){
+    super(props)
+    this.state = {
+      modalProperties: {
+        onboardingDeskModal: 'ONBOARDING_DESK_MODAL'
+      }
+    }
+  }
 
   render() {
+    const { onboardingDeskModal } = this.state.modalProperties
     return (
       <div>
-        <OnboardingStepper />
+        <div>
+          <OnboardingStepper />
+        </div>
+        <div className=''>
+
+          <ButtonModal usedStyle='' label='Start onboard desk' Onboard modal={ onboardingDeskModal } />
+        </div>
       </div>
     )
   }
