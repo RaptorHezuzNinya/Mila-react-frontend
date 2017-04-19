7bf91ade822a52ca0bcfdc6f884a03ed47619ab1
raptorzhezuzninya <w22243vrh@gmail.com>
2017-04-19 17:23:04 +0200

    removed log cleanup

--- src/onboarding/OnboardingStepper.js
+++ src/onboarding/OnboardingStepper.js
@@ -137,7 +137,6 @@ class OnboardingStepper extends PureComponent {
   render() {
     const { stepIndex, stepperwidth } = this.state;
     const { orientation } = this.props
-    console.log('STEPINDEX:', stepIndex)
     return (
       <div className={ stepIndex === 1 ? 'step-wrap-step1-desk' : 'stepper-wrapper' }>
         <Stepper activeStep={stepIndex} orientation={orientation} style={styles.stepper} className="stepper">
