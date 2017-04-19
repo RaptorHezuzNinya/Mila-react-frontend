fd31609cb22b6db506add7249225dbad2519763f
raptorzhezuzninya <w22243vrh@gmail.com>
2017-04-19 17:07:48 +0200

    add h3 to startsorting party so i can reuse sass

--- src/onboarding/ScanningInbox.js
+++ src/onboarding/ScanningInbox.js
@@ -3,7 +3,7 @@ import React from 'react'
 const ScanningInbox = () => {
   return (
     <div className='scanning-inbox'>
-      <h1>{"Hi #{username}!"}</h1>
+      <h3>{"Hi #{username}!"}</h3>
       <p className='parag-one'>I am Mila. Nice to meet you.</p>
       <p className='parag-two'>I’ve already started scanning your inbox in the background. From now on, it’s my job to keep track of all the valuable contacts in your inbox.
       </p>
