99e1420b961c543609662f6ef36d92ca9ef94fb7
raptorzhezuzninya <w22243vrh@gmail.com>
2017-04-20 18:37:51 +0200

    add newly created menudrauwer to settings acocunt

--- src/settingsAccount/SettingsAccount.js
+++ src/settingsAccount/SettingsAccount.js
@@ -1,9 +1,11 @@
 import React, { PureComponent } from 'react'
-
+import MenuDrawer from '../components/MenuDrawer'
 class SettingsAccount extends PureComponent {
   render () {
     return (
-      <div>iam the accountsettings page w/e</div>
+      <div>
+        <MenuDrawer />
+      </div>
     )
   }
 }
