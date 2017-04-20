8a071163e178f5d626dc11b8d02b13f55ec732f6
raptorzhezuzninya <w22243vrh@gmail.com>
2017-04-20 16:12:52 +0200

    add 2 new action for addlistcount

--- src/actions/onboarding.js
+++ src/actions/onboarding.js
@@ -1,5 +1,5 @@
 export const INCR_STEP_INDEX = 'INCR_STEP_INDEX'
-export const addStepIndex = (stepIndex) => {
+export const incrStepIndex = (stepIndex) => {
   return {
     type: INCR_STEP_INDEX,
     payload: stepIndex
@@ -7,9 +7,25 @@ export const addStepIndex = (stepIndex) => {
 }
 
 export const DECR_STEP_INDEX = 'DECR_STEP_INDEX'
-export const reduceStepIndex = (stepIndex) => {
+export const decrStepIndex = (stepIndex) => {
   return {
     type: DECR_STEP_INDEX,
     payload: stepIndex
+  }
+}
+
+export const INCR_LIST_COUNT = 'INCR_LIST_COUNT'
+export const incrListCount = (listCount) => {
+  return {
+    type: INCR_LIST_COUNT,
+    payload: listCount
+  }
+}
+
+export const DECR_LIST_COUNT = 'DECR_LIST_COUNT'
+export const decrListCount = (listCount) => {
+  return {
+    type: DECR_LIST_COUNT,
+    payload: listCount
   }
 }
