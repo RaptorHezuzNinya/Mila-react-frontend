98c6bc3dbb0ce03fba283632666f3958a7eca158
raptorzhezuzninya <w22243vrh@gmail.com>
2017-04-20 14:34:07 +0200

    add initial state

--- src/reducers/onboarding.js
+++ src/reducers/onboarding.js
@@ -1,5 +1,12 @@
 
-export default function onboarding(state = {}, {type, payload} = {}) {
+
+const initialState = {
+  stepIndex: 0,
+  listCount: 0,
+  ProceedWarning: false
+}
+
+export default function onboarding(state = initialState, {type, payload} = {}) {
   switch (type) {
     case expression:
 
