a2ba96fe4020e96e9527c3ee55be936b10fa6fa3
raptorzhezuzninya <w22243vrh@gmail.com>
2017-04-20 16:47:18 +0200

    set proceedwarning initial state to false add show hide case

--- src/reducers/onboarding.js
+++ src/reducers/onboarding.js
@@ -1,8 +1,9 @@
-import { INCR_STEP_INDEX, DECR_STEP_INDEX, INCR_LIST_COUNT, DECR_LIST_COUNT } from '../actions/onboarding'
+import { INCR_STEP_INDEX, DECR_STEP_INDEX, INCR_LIST_COUNT, DECR_LIST_COUNT, SHOW_PROCEED_WARN, HIDE_PROCEED_WARN  } from '../actions/onboarding'
 
 const initialState = {
   stepIndex: 0,
   listCount: 0,
+  proceedWarning: false
 }
 
 export default function onboarding(state = initialState, {type, payload} = {}) {
@@ -20,6 +21,11 @@ export default function onboarding(state = initialState, {type, payload} = {}) {
     case DECR_LIST_COUNT:
       return Object.assign({}, state, {listCount: payload - 1})
 
+    case SHOW_PROCEED_WARN:
+      return Object.assign({}, state, {proceedWarning: true})
+
+    case HIDE_PROCEED_WARN:
+      return Object.assign({}, state, {proceedWarning: false})
 
     default:
       return state
