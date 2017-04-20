4e39e62f78a849d138eb4afe7b650ae8a6602c38
raptorzhezuzninya <w22243vrh@gmail.com>
2017-04-20 17:12:44 +0200

    add new accountsettings folder

---
+++ src/accountSettings/AccountSettings.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react'
+
+class AccountSettings extends PureComponent {
+  render () {
+    return (
+      <div>iam the accountsettings page w/e</div>
+    )
+  }
+}
+
+export default AccountSettings
