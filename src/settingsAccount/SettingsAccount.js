a8edd661327bc5d85c6d3a49e8fd3a763239cee3
raptorzhezuzninya <w22243vrh@gmail.com>
2017-04-20 17:29:51 +0200

    renamed folder this is more descriptive

---
+++ src/settingsAccount/SettingsAccount.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react'
+
+class SettingsAccount extends PureComponent {
+  render () {
+    return (
+      <div>iam the accountsettings page w/e</div>
+    )
+  }
+}
+
+export default SettingsAccount
