a733431b8f0c6d59b39b4cf985b05af0b58f7d20
raptorzhezuzninya <w22243vrh@gmail.com>
2017-04-20 16:14:18 +0200

    add listcount to reducer

--- src/reducers/onboarding.js
+++ src/reducers/onboarding.js
@@ -1,7 +1,8 @@
-import { INCR_STEP_INDEX, DECR_STEP_INDEX } from '../actions/onboarding'
+import { INCR_STEP_INDEX, DECR_STEP_INDEX, INCR_LIST_COUNT, DECR_LIST_COUNT } from '../actions/onboarding'
 
 const initialState = {
   stepIndex: 0,
+  listCount: 0,
 }
 
 export default function onboarding(state = initialState, {type, payload} = {}) {
@@ -12,6 +13,13 @@ export default function onboarding(state = initialState, {type, payload} = {}) {
 
     case DECR_STEP_INDEX:
       return Object.assign({}, state, {stepIndex: payload - 1})
+
+    case INCR_LIST_COUNT:
+      return Object.assign({}, state, {listCount: payload + 1})
+
+    case DECR_LIST_COUNT:
+      return Object.assign({}, state, {listCount: payload - 1})
+
 
     default:
       return state
