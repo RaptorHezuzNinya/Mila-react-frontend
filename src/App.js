85544b67a28b47955c14f6808b8f9fb687302462
raptorzhezuzninya <w22243vrh@gmail.com>
2017-04-22 23:55:33 +0200

    add menudrawer to app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 // import { Link } from 'react-router'
 import Navigation from './components/Navigation'
+import MenuDrawer from './components/MenuDrawer'
 import ModalRoot from './modals/ModalRoot'
 
 // Material UI Components
@@ -10,6 +11,14 @@ import muiTheme from './assets/styles/theme'
 
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      openDrawer: false
+    }
+    this.handleOpenMenuDrawer = this.handleOpenMenuDrawer.bind(this)
+  }
+
   static childContextTypes = {
     muiTheme: React.PropTypes.object.isRequired,
   }
