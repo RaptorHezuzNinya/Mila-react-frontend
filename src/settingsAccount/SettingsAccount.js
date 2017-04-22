7f601035dfa11ea1fe22af43bf6c6fcc8cd78eaa
raptorzhezuzninya <w22243vrh@gmail.com>
2017-04-22 19:13:19 +0200

    add flatbut to click handle open

--- src/settingsAccount/SettingsAccount.js
+++ src/settingsAccount/SettingsAccount.js
@@ -1,10 +1,34 @@
 import React, { PureComponent } from 'react'
 import MenuDrawer from '../components/MenuDrawer'
+import FlatButton from 'material-ui/FlatButton'
+// style
+import './SettingsAccount.sass'
+
 class SettingsAccount extends PureComponent {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      openDrawer: false
+    }
+    this.handleOpen = this.handleOpen.bind(this)
+  }
+
+  handleOpen () {
+    console.log('clicked')
+    this.setState({
+      openDrawer: !this.state.open
+    })
+  }
+
   render () {
     return (
       <div>
-        <MenuDrawer />
+        <div className='open-btn'>
+          <FlatButton label='open' onClick={this.handleOpen} />
+        </div>
+        <MenuDrawer open={this.state.openDrawer}/>
+
       </div>
     )
   }
