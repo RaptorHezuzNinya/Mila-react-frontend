652b10ad958d4c9043f0fae857f1329ebca1fa27
raptorzhezuzninya <w22243vrh@gmail.com>
2017-04-25 01:43:58 +0200

    add markup for needed fields

--- src/settingsAccount/UserDetails.js
+++ src/settingsAccount/UserDetails.js
@@ -6,31 +6,59 @@ import './UserDetails.sass'
 
 class UserDetails extends PureComponent {
   render () {
+    const styles = {
+      hintText: {
+        marginLeft: 40
+      },
+      inputStyle: {
+        marginLeft: 40
+      }
+    }
     return (
       <div className='user-details-wrapper'>
-        <div className='firstname-holder'>
-          <TextField placeholder='First name' fullWidth={true}/>
-        </div>
-        <div className='lastname-holder'>
-          <TextField placeholder='Last name' fullWidth={true}/>
-        </div>
-        <div className='email-holder'>
-          <TextField placeholder='Email' fullWidth={true}/>
-        </div>
+        <form onSubmit={this.handleFormSubmit}>
 
-        <div className='slack-holder'>
-          <p className='slack-text'>Slack Notifications</p>
-          <Toggle className='slack-slider'/>
-        </div>
-        <div className='email-holder'>
-          <p className='email-text'>Email Notifications</p>
-          <Toggle className='email-slider'/>
-        </div>
+          <div className='details-holder'>
+            <div className='firstname-holder'>
+              <TextField
+                hintText='First name'
+                hintStyle={styles.hintText}
+                inputStyle={styles.inputStyle}
+                fullWidth={true}/>
+            </div>
+            <div className='lastname-holder'>
+              <TextField
+                hintText='Last name'
+                hintStyle={styles.hintText}
+                inputStyle={styles.inputStyle}
+                fullWidth={true}/>
+            </div>
+            <div className='email-holder'>
+              <TextField
+                hintText='Email'
+                hintStyle={styles.hintText}
+                inputStyle={styles.inputStyle}
+                fullWidth={true}/>
+            </div>
+          </div>
+
+          <div className='notification-holder'>
+            <div className='slack-slider-holder'>
+              <p className='slack-text'>Slack Notifications</p>
+              <Toggle className='slack-slider'/>
+            </div>
+            <div className='email-slider-holder'>
+              <p className='email-text'>Email Notifications</p>
+              <Toggle className='email-slider'/>
+            </div>
+          </div>
 
 
-        <div className='submit-holder'>
-          <FlatButton className='btn-green' label='save'/>
-        </div>
+          <div className='submit-holder'>
+            <FlatButton className='btn-green' label='save' type='submit'/>
+          </div>
+
+        </form>
       </div>
     )
   }
