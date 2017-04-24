bf2c440c1f48eb51a11b7c30f7c9f9e1b2ec638b
raptorzhezuzninya <w22243vrh@gmail.com>
2017-04-24 22:17:13 +0200

    add h2 and h3 to pagetitle comp add attributes

--- src/components/PageTitle.js
+++ src/components/PageTitle.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import './PageTitle.sass'
 
 const PageTitle = (props) => {
   return (
-    <div className={props.pageTitleClassName}>
-      <h2>{props.pageTitleContent}</h2>
+    <div className={props.titleClassName}>
+      <h2>{props.pageTitleContentH2}</h2>
+      <h3>{props.pageTitleContentH3}</h3>
     </div>
   )
 }
