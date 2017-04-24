c026d2bff53b1898af6256d5fb2a240b2a563c07
raptorzhezuzninya <w22243vrh@gmail.com>
2017-04-25 00:01:17 +0200

    add fullwidth attr

--- src/settingsAccount/UserDetails.js
+++ src/settingsAccount/UserDetails.js
@@ -1,22 +1,22 @@
 import React, {PureComponent} from 'react'
-// material-ui components
 import TextField from 'material-ui/TextField'
 import FlatButton from 'material-ui/FlatButton'
+import './UserDetails.sass'
 
 class UserDetails extends PureComponent {
   render () {
     return (
       <div className='user-details-wrapper'>
         <div className='firstname-holder'>
-          <TextField placeholder='First name'/>
+          <TextField placeholder='First name' fullWidth={true}/>
         </div>
         <div className='lastname-holder'>
-          <TextField placeholder='Last name'/>
+          <TextField placeholder='Last name' fullWidth={true}/>
         </div>
         <div className='email-holder'>
-          <TextField placeholder='Email'/>
+          <TextField placeholder='Email' fullWidth={true}/>
         </div>
-        <div>
+        <div className='submit-holder'>
           <FlatButton label='save'/>
         </div>
       </div>
