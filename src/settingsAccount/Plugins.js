596d51dd64774c5fd7b18b63db782bf160e70c76
raptorzhezuzninya <w22243vrh@gmail.com>
2017-04-25 13:31:55 +0200

    add props for classnames

--- src/settingsAccount/Plugins.js
+++ src/settingsAccount/Plugins.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import FlatButton from 'material-ui/FlatButton'
-
+import './Plugins.sass'
 const Plugins = (props) => {
   return (
-    <div className={props.pluginWrapClassName}>
+    <div className={props.pluginHolderClassName}>
       <p className={props.textClassName}>Get notified of new contacts, download plugin:</p>
-      <FlatButton label={props.gmailBtnlabel} onClick={() => console.log('email btn clicked')}/>
-      <FlatButton label={props.slackBtnlabel} onClick={() => console.log('slack btn clicked')}/>
+      <div className={props.btnHolderClassName}>
+        <FlatButton label={props.gmailBtnlabel} className='btn-grey' onClick={() => console.log('email btn clicked')}/>
+        <FlatButton label={props.slackBtnlabel} className='btn-grey' onClick={() => console.log('slack btn clicked')}/>
+      </div>
     </div>
   )
 }
