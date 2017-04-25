1e28c697a1ec662a97442633a18e7b15f79ffb49
raptorzhezuzninya <w22243vrh@gmail.com>
2017-04-25 13:43:21 +0200

    create to new files secureserver comp and style sheet

---
+++ src/components/SecureServer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const SecureServer = (props) => {
+  return (
+    <div className='sercure-server-holder'>
+      <h3>Secure Servers</h3>
+      <p>About your data</p>
+      <p>
+        We have high respect for privacy and security. We believe your data will always belong to you and to you only. We use high industry standard encryption methods to secure our servers that handle personal data.
+        <br></br>
+        <br></br>Read our privacy and security statement.
+      </p>
+    </div>
+  )
+}
+
+export default SecureServer
