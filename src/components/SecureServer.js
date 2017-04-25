f3618dc1adfe92248001a55614a54b1803aa409b
raptorzhezuzninya <w22243vrh@gmail.com>
2017-04-25 13:56:41 +0200

    add classnames to secureserver comp

--- src/components/SecureServer.js
+++ src/components/SecureServer.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import './SecureServer.sass'
 
-const SecureServer = (props) => {
+const SecureServer = () => {
   return (
-    <div className='sercure-server-holder'>
-      <h3>Secure Servers</h3>
-      <p>About your data</p>
-      <p>
+    <div className='secure-server-holder'>
+      <h3 className='secure-header'>Secure Servers</h3>
+      <p className='secure-intro'>About your data</p>
+      <p className='secure-text'>
         We have high respect for privacy and security. We believe your data will always belong to you and to you only. We use high industry standard encryption methods to secure our servers that handle personal data.
         <br></br>
         <br></br>Read our privacy and security statement.
