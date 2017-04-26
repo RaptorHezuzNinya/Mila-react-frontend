3492eb3829c86215e6138c5bee895036b1ae530e
raptorzhezuzninya <w22243vrh@gmail.com>
2017-04-26 15:17:47 +0200

    create new folder for new main route component

---
+++ src/settingsList/SettingsList.js
@@ -0,0 +1,11 @@
+import React, { PureComponent, PropTypes } from 'react'
+
+class SettingsList extends PureComponent {
+  render () {
+    return (
+      <div>settingsLists comp</div>
+    )
+  }
+}
+
+export default SettingsList
