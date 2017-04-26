0ff21d630de1e5e21d866085ae365a0d177f2790
raptorzhezuzninya <w22243vrh@gmail.com>
2017-04-26 17:44:51 +0200

    add the initial networklist data

--- src/reducers/networklists.js
+++ src/reducers/networklists.js
@@ -2,30 +2,26 @@ import { CREATE_NETWORKLIST, UPDATE_NETWORKLIST, DELETE_NETWORKLIST } from '../a
 
 
 const initialState = [
-  // {
-  //   id: 1,
-  //   title: 'Business',
-  //   avatar: '~/assets/images/avatars/troll.png',
-  //   description: 'The VIP list is used for people who have asked questions about our product and want to try our next update'
-  // },
-  // {
-  //   id: 3,
-  //   title: 'Business Int',
-  //   avatar: '~/assets/images/avatars/troll.png',
-  //   description: 'The VIP list is used for people who have asked questions about our product and want to try our next update'
-  // },
-  // {
-  //   id: 4,
-  //   title: 'Friends',
-  //   avatar: '~/assets/images/avatars/troll.png',
-  //   description: 'The VIP list is used for people who have asked questions about our product and want to try our next update'
-  // },
-  // {
-  //   id: 5,
-  //   title: 'All',
-  //   avatar: '~/assets/images/avatars/troll.png',
-  //   description: 'The VIP list is used for people who have asked questions about our product and want to try our next update'
-  // },
+  {
+    id: 1,
+    title: 'Business',
+    description: 'The VIP list is used for people who have asked questions about our product and want to try our next update'
+  },
+  {
+    id: 3,
+    title: 'Business Int',
+    description: 'The VIP list is used for people who have asked questions about our product and want to try our next update'
+  },
+  {
+    id: 4,
+    title: 'Friends',
+    description: 'The VIP list is used for people who have asked questions about our product and want to try our next update'
+  },
+  {
+    id: 5,
+    title: 'All',
+    description: 'The VIP list is used for people who have asked questions about our product and want to try our next update'
+  },
 ]
 
 export default function networklists(state = initialState, { type, payload } = {}) {
