3493d39e827e74465d308be2bd52fe81f69551f8
raptorzhezuzninya <w22243vrh@gmail.com>
2017-04-28 14:44:52 +0200

    add onclick handlers to buttons

--- src/settingsList/EditNetworkList.js
+++ src/settingsList/EditNetworkList.js
@@ -1,9 +1,16 @@
 import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
+import { deleteNetworkList, updateNetworkList } from '../actions/networklists'
 import FlatButton from 'material-ui/FlatButton'
 import NetworkIcon from 'material-ui/svg-icons/action/view-list'
 import './EditNetworkList.sass'
 class EditNetworkList extends PureComponent {
+
+  handleDeleteListClick(networkList) {
+    console.log(networkList)
+    // this.props.deleteNetworkList({networkList})
+
+  }
 
   renderNetworkLists () {
     const { networkLists } = this.props
@@ -11,15 +18,14 @@ class EditNetworkList extends PureComponent {
       return (
         <div className='list-item-holder' key={networkList.id}>
           <div className='list-title'>
-            <span>
+            <span className='list-icon'>
               <NetworkIcon color='#52be9c'/>
             </span>
             {networkList.title}
           </div>
           <div className='list-btns'>
-            <FlatButton label='SHOW' />
-            <FlatButton label='EDIT' />
-            <FlatButton label='DELETE' />
+            <FlatButton label='EDIT' primary={true} onClick={this.props.updateNetworkList.bind(this, networkList.title)} />
+            <FlatButton label='DELETE' primary={true} onClick={this.props.deleteNetworkList.bind(this, networkList.title)} />
           </div>
         </div>
       )
@@ -29,9 +35,7 @@ class EditNetworkList extends PureComponent {
   render () {
     return (
       <div className='edit-networklist-holder'>
-
         {this.renderNetworkLists()}
-
       </div>
     )
   }
