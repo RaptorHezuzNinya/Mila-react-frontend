ad9e62e963d4df472ea048192e2157531d0ea9b4
raptorzhezuzninya <w22243vrh@gmail.com>
2017-04-29 01:15:48 +0200

    fixed wrong double import

--- src/onboarding/CreateListsContainer.js
+++ src/onboarding/CreateListsContainer.js
@@ -1,7 +1,6 @@
 import React, { PureComponent, PropTypes } from 'react'
 import { connect } from 'react-redux'
-import { Field, reduxForm } from 'redux-form'
-import { reset } from 'redux-form'
+import { Field, reduxForm, reset } from 'redux-form'
 import Media from 'react-media'
 import { createNetworkList, deleteNetworkList } from '../actions/networklists'
 import ResponsiveContent from './ResponsiveContent'
