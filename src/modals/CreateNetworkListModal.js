f0d93d45b09763f31899175b50aef5a873b3c835
raptorzhezuzninya <w22243vrh@gmail.com>
2017-05-01 19:42:56 +0200

    replaced double quotes

--- src/modals/CreateNetworkListModal.js
+++ src/modals/CreateNetworkListModal.js
@@ -65,58 +65,58 @@ class CreateNetworkListModal extends PureComponent {
   render(){
     const { title, description } = this.props
     return (
-      <div className="modal-wrapper">
+      <div className='modal-wrapper'>
         <Dialog
-          className="createlist-dialog"
+          className='createlist-dialog'
           modal={true}
           contentStyle={styles.customContentStyle}
           open={this.state.open}
         >
-          <div className="content-wrapper">
-            <div className="cl-header">
+          <div className='content-wrapper'>
+            <div className='cl-header'>
               <h2>Create a new Mila list</h2>
               <h3>Use lists to group your contacts</h3>
             </div>
-            <div className="listname-container">
-              <p className="list-name">LIST NAME</p>
-              <p className="char-count"> {maxTitleCount - this.state.title.length} left</p>
+            <div className='listname-container'>
+              <p className='list-name'>LIST NAME</p>
+              <p className='char-count'> {maxTitleCount - this.state.title.length} left</p>
               <TextField hintStyle={styles.hintStyle}
-                         className="list-input"
-                         name="title"
-                         hintText="Type a list name, e.g. Top clients, freelancers, potential investors"
+                         className='list-input'
+                         name='title'
+                         hintText='Type a list name, e.g. Top clients, freelancers, potential investors'
                          value={this.state.title || ''}
                          onChange={ this.handleChange.bind(this) }
-                         maxLength="25">
+                         maxLength='25'>
               </TextField>
             </div>
-            <div className="descript-container">
-              <p className="desc-name">DESCRIPTION (optional)</p>
-              <p className="char-count"> {maxDescCount - this.state.description.length} left</p>
+            <div className='descript-container'>
+              <p className='desc-name'>DESCRIPTION (optional)</p>
+              <p className='char-count'> {maxDescCount - this.state.description.length} left</p>
               <TextField hintStyle={styles.hintStyle}
-                         className="list-input"
-                         name="description"
-                         hintText="What do youdo with this list? e.g. The VIP list is used for people who have asked questions about our product and want to try our next update"
+                         className='list-input'
+                         name='description'
+                         hintText='What do youdo with this list? e.g. The VIP list is used for people who have asked questions about our product and want to try our next update'
                          multiLine={true}
                          rows={2}
                          rowsMax={4}
                          value={this.state.description || ''}
                          onChange={ this.handleChange.bind(this) }
-                         maxLength="250">
+                         maxLength='250'>
               </TextField>
             </div>
-            <div className="cta-container">
-              <CheckBox className="team-checkb" label="Share this list with your team."/>
-              <div className="submit-but">
+            <div className='cta-container'>
+              <CheckBox className='team-checkb' label='Share this list with your team.'/>
+              <div className='submit-but'>
                 <FlatButton
-                  label="CREATE"
+                  label='CREATE'
                   primary={true}
                   disabled={false}
                   onClick={this.createNetworkList.bind(this)}
                 />
               </div>
-              <div className="cancel-but">
+              <div className='cancel-but'>
                 <FlatButton
-                  label="Cancel"
+                  label='Cancel'
                   primary={true}
                   onTouchTap={this.handleClose}
                 />
