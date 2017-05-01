4efc33eae3f69f363956b035c36b9a6ac69956d0
raptorzhezuzninya <w22243vrh@gmail.com>
2017-05-01 19:49:17 +0200

    importing pagetitle fixed indentation

--- src/modals/CreateNetworkListModal.js
+++ src/modals/CreateNetworkListModal.js
@@ -2,6 +2,7 @@ import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
 import { hideModal } from '../actions/modals/index'
 import { createNetworkList } from '../actions/networklists'
+import PageTitle from '../components/PageTitle'
 import Dialog from 'material-ui/Dialog'
 import FlatButton from 'material-ui/FlatButton'
 import RaisedButton from 'material-ui/RaisedButton'
@@ -73,35 +74,38 @@ class CreateNetworkListModal extends PureComponent {
           open={this.state.open}
         >
           <div className='content-wrapper'>
-            <div className='cl-header'>
-              <h2>Create a new Mila list</h2>
-              <h3>Use lists to group your contacts</h3>
-            </div>
+            <PageTitle
+              titleClassName='createlist-modal-title'
+              pageTitleContentH2='Create a new Mila list'
+              pageTitleContentH3='Use lists to group your contacts'/>
+
             <div className='listname-container'>
               <p className='list-name'>LIST NAME</p>
               <p className='char-count'> {maxTitleCount - this.state.title.length} left</p>
-              <TextField hintStyle={styles.hintStyle}
-                         className='list-input'
-                         name='title'
-                         hintText='Type a list name, e.g. Top clients, freelancers, potential investors'
-                         value={this.state.title || ''}
-                         onChange={ this.handleChange.bind(this) }
-                         maxLength='25'>
+              <TextField
+                hintStyle={styles.hintStyle}
+                className='list-input'
+                name='title'
+                hintText='Type a list name, e.g. Top clients, freelancers, potential investors'
+                value={this.state.title || ''}
+                onChange={ this.handleChange.bind(this) }
+                maxLength='25'>
               </TextField>
             </div>
             <div className='descript-container'>
               <p className='desc-name'>DESCRIPTION (optional)</p>
               <p className='char-count'> {maxDescCount - this.state.description.length} left</p>
-              <TextField hintStyle={styles.hintStyle}
-                         className='list-input'
-                         name='description'
-                         hintText='What do youdo with this list? e.g. The VIP list is used for people who have asked questions about our product and want to try our next update'
-                         multiLine={true}
-                         rows={2}
-                         rowsMax={4}
-                         value={this.state.description || ''}
-                         onChange={ this.handleChange.bind(this) }
-                         maxLength='250'>
+              <TextField
+                hintStyle={styles.hintStyle}
+                className='list-input'
+                name='description'
+                hintText='What do youdo with this list? e.g. The VIP list is used for people who have asked questions about our product and want to try our next update'
+                multiLine={true}
+                rows={2}
+                rowsMax={4}
+                value={this.state.description || ''}
+                onChange={ this.handleChange.bind(this) }
+                maxLength='250'>
               </TextField>
             </div>
             <div className='cta-container'>
