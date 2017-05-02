7fd0ff9e5914731cde0f8fac1051e7072fd0f9d9
raptorzhezuzninya <w22243vrh@gmail.com>
2017-05-02 15:06:03 +0200

    cleaned render method add validation add reduxForm to connect

--- src/modals/CreateNetworkListModal.js
+++ src/modals/CreateNetworkListModal.js
@@ -54,114 +54,115 @@ class CreateNetworkListModal extends PureComponent {
     super()
     this.state = {
       open: true,
-      title: '',
-      description: '',
     }
   }
 
-  handleChange = (event) => {
-    const name = event.target.name
-    const value = event.target.value
 
-    this.setState({
-      [name]: value
+  handleClose = () => {
+    this.setState({open: !this.state.open})
+    this.props.hideModal()
+  }
+
+  onSubmit (formProps) {
+    console.log('submitted')
+    this.props.createNetworkList(formProps)
+    this.handleClose()
+  }
+
+  renderTextField = ({ input, maxLength, label, meta: { touched, error } }) => (
+    <TextField
+      name={name}
+      hintText={label}
+      hintStyle={styles.hintStyle}
+      fullWidth={true}
+      inputStyle={styles.inputStyle}
+      errorText={touched && error}
+      maxLength={maxLength}
+      {...input}
+    />
+  )
+
+  renderFormFields = () => {
+    return formData.map((form) => {
+      return (
+        <div key={form.name}>
+          {form.formHeader}
+          <Field
+            validate={[ maxLength15 ]}
+            name={form.name}
+            label={form.label}
+            component={ this.renderTextField } />
+        </div>
+      )
     })
   }
 
 
-  createNetworkList(event){
-    event.preventDefault()
-    const { title, description } = this.state
-    this.props.createNetworkList({title, description})
-    this.handleClose()
-  }
-
-  handleClose = () => {
-    this.setState({open: false})
-    this.props.hideModal()
-  }
-
-  // handleOpen = () => {
-  //   this.setState({open: true});
-  // };
-
   render(){
-    const { title, description } = this.props
+    const { title, description, handleSubmit } = this.props
+    console.log('name props', this.props.networkListFormValue)
     return (
       <div className='modal-wrapper'>
         <Dialog
           className='createlist-dialog'
-          autoScrollBodyContent={true}
           modal={true}
           contentStyle={styles.customContentStyle}
-          open={this.state.open}
-        >
+          open={this.state.open} >
           <div className='content-wrapper'>
             <PageTitle
               titleClassName='createlist-modal-title'
               pageTitleContentH2='Create a new Mila list'
               pageTitleContentH3='Use lists to group your contacts' />
 
-            {/* <div className='listname-container'>
-              <p className='list-name'>LIST NAME</p>
-              <p className='char-count'> {maxTitleCount - this.state.title.length} left</p>
 
-              <TextField
-                hintStyle={styles.hintStyle}
-                className='list-input'
-                name='title'
-                hintText='Type a list name, e.g. Top clients, freelancers, potential investors'
-                value={this.state.title || ''}
-                onChange={ this.handleChange.bind(this) }
-                maxLength='25'>
-              </TextField>
-
-            </div>
-            <div className='descript-container'>
-              <p className='desc-name'>DESCRIPTION (optional)</p>
-              <p className='char-count'> {maxDescCount - this.state.description.length} left</p>
-              <TextField
-                hintStyle={styles.hintStyle}
-                className='list-input'
-                name='description'
-                hintText='What do youdo with this list? e.g. The VIP list is used for people who have asked questions about our product and want to try our next update'
-                multiLine={true}
-                rows={2}
-                rowsMax={4}
-                value={this.state.description || ''}
-                onChange={ this.handleChange.bind(this) }
-                maxLength='250'>
-              </TextField>
-            </div> */}
-            <NetworkListForm
-              fieldHolder='NWL-modal-field-holder'
-              btnLabel='create'
-              btnClass='btn-grey-wide'
-              btnHolder='NWL-modal-btnholder'
-              handleCloseModal={this.handleClose}/>
-            <div className='cta-container'>
-              <CheckBox className='team-checkb' label='Share this list with your team.'/>
-              {/* <div className='submit-but'>
-                <FlatButton
-                  label='CREATE'
-                  primary={true}
-                  disabled={false}
-                  onClick={this.createNetworkList.bind(this)}
-                />
-              </div> */}
-              <div className='cancel-but'>
-                <FlatButton
-                  label='Cancel'
-                  primary={true}
-                  onTouchTap={this.handleClose}
-                />
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+              <div className='listname-container'>
+                { this.renderFormFields() }
               </div>
-            </div>
+              <div className='descript-container'>
+
+
+              </div>
+              <div className='cta-container'>
+
+                <CheckBox className='team-checkb' label='Share this list with your team.' />
+
+                <div className='submit-but'>
+                  <FlatButton
+                    type='submit'
+                    label='CREATE'
+                    disabled={false} />
+                </div>
+                <div className='cancel-but'>
+                  <FlatButton
+                    label='Cancel'
+                    onTouchTap={this.handleClose} />
+                </div>
+              </div>
+            </form>
           </div>
         </Dialog>
       </div>
     )
   }
 }
+const validate = (values) => {
+  const errors = {}
+  const requiredFields = [ 'title', 'description']
+  requiredFields.forEach( (field) => {
+    if (!values[ field ]) {
+      errors[ field ] = 'Required'
+    }
+  })
+  return errors
+}
 
-export default connect(null, { hideModal, createNetworkList } ) (CreateNetworkListModal)
+const afterSubmit = (result, dispatch) => {
+  dispatch(reset('createNWLModalForm'))
+}
+
+export default connect(null, { hideModal, createNetworkList } )(reduxForm({
+  form: 'createNWLModalForm',
+  validate,
+  onSubmitSuccess: afterSubmit
+})(CreateNetworkListModal))
