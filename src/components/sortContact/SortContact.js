f0a97204a5b883d24f71de4029a5cecf96d9c5e1
raptorzhezuzninya <w22243vrh@gmail.com>
2017-05-03 17:14:45 +0200

    create new main forlder for sortcontact main folder

---
+++ src/components/sortContact/SortContact.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react'
+
+class SortContact extends PureComponent {
+  render () {
+    return (
+      <div className='sort-contact-wrapper'>
+        SortContact
+      </div>
+    )
+  }
+}
+
+export default SortContact
