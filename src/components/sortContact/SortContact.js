9b77989d761eb35599c32694a452a4edf50cf725
raptorzhezuzninya <w22243vrh@gmail.com>
2017-05-03 21:28:28 +0200

    rendering contact card in sortcontact main comp:

--- src/components/sortContact/SortContact.js
+++ src/components/sortContact/SortContact.js
@@ -1,10 +1,11 @@
 import React, { PureComponent } from 'react'
+import ContactCard from './ContactCard'
 
 class SortContact extends PureComponent {
   render () {
     return (
       <div className='sort-contact-wrapper'>
-        SortContact
+        <ContactCard />
       </div>
     )
   }
