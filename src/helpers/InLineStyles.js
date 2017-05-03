7f62233182fdf20363d39b143714f57cb8273e20
raptorzhezuzninya <w22243vrh@gmail.com>
2017-05-03 15:20:15 +0200

    create new folder for helper files for inline styles en form data etc

---
+++ src/helpers/InLineStyles.js
@@ -0,0 +1,26 @@
+// imported in ~/onboarding/CreateListsContainer.js
+export const inlineCreateListsContainerStyles = {
+  hint: {
+    fontSize: 14,
+    fontFamily: 'Montserrat-Light'
+  },
+  inputStyle: {
+    fontSize: 15,
+    fontFamily: 'Montserrat-Light',
+    color: '#292f36',
+  }
+}
+// used in ~/components/NetworkListForm.js
+export const inlineNetworkListFormStyles = {
+  hint: {
+    fontSize: 14,
+    fontFamily: 'Montserrat-Light',
+    marginLeft: 15,
+  },
+  inputStyle: {
+    fontSize: 15,
+    fontFamily: 'Montserrat-Light',
+    marginLeft: 15,
+    color: '#292f36',
+  }
+}
