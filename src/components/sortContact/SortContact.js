579e9d5d5ac3bec7f2e393cadce85d5e25aee102
raptorzhezuzninya <w22243vrh@gmail.com>
2017-05-03 22:05:50 +0200

    rendering new progress component in sortcontact

--- src/components/sortContact/SortContact.js
+++ src/components/sortContact/SortContact.js
@@ -1,11 +1,20 @@
 import React, { PureComponent } from 'react'
 import ContactCard from './ContactCard'
+import ProgressIndicator from './ProgressIndicator'
+
+import './SortContact.sass'
 
 class SortContact extends PureComponent {
   render () {
     return (
       <div className='sort-contact-wrapper'>
-        <ContactCard />
+        <div className='linear-progress-wrapper'>
+          <ProgressIndicator
+            mode='determinate' />
+        </div>
+        <div className='contact-card-wrapper'>
+          <ContactCard />
+        </div>
       </div>
     )
   }
