1486ad5cd9c52621ed6e907414507a8958f61d2e
raptorzhezuzninya <w22243vrh@gmail.com>
2017-05-03 17:14:15 +0200

    add new path for sort contact

--- src/index.js
+++ src/index.js
@@ -5,17 +5,15 @@ import { Router, Route, IndexRoute } from 'react-router'
 import store, { history } from './store'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 injectTapEventPlugin()
-
 import routes from './middleware/routes'
-// Components
 import App from './App'
 import Onboarding from './components/onboarding/Onboarding'
 import NewContactsContainer from './components/newContacts/NewContactsContainer'
 import Contact from './components/newContacts/Contact'
+import SortContact from './components/sortContact/SortContact'
 import Network from './components/myNetwork/Network'
 import SettingsAccount from './components/settingsAccount/SettingsAccount'
 import SettingsList from './components/settingsList/SettingsList'
-// base styles
 import './assets/styles/main.sass'
 import 'normalize.css'
 import './assets/styles/base/basestyles.sass'
@@ -28,6 +26,7 @@ ReactDOM.render(
         <IndexRoute component={Network} />
         <Route path={routes.onboardingPath} component={Onboarding} />
         <Route path={routes.newContactsPath} component={NewContactsContainer} />
+        <Route path={routes.sortContactPath} component={SortContact} />
         <Route path={routes.myNetworkPath} component={Network} />
         <Route path={routes.settingsAccountPath} component={SettingsAccount} />
         <Route path={routes.settingsListPath} component={SettingsList} />
