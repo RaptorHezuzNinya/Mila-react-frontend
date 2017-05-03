44a60c892fe37679d3408fa81216e8cc26343045
raptorzhezuzninya <w22243vrh@gmail.com>
2017-05-03 15:49:35 +0200

    changed the formdata object to func cuz i need variables in there

--- src/helpers/formData.js
+++ src/helpers/formData.js
@@ -1,28 +1,30 @@
+// React must be in scope if you use jsx
 import React from 'react'
 
 // // used in ~/modals/CreateNetworkList.js
-// export const formDataCreateNetworkListModal = [
-//   {
-//     name: 'title',
-//     label: 'Type a list name, e.g. Top clients, freelancers, potential investors',
-//     formHeader: <div className='title-details'><p className='list-name'>Your list title</p><p>{maxTitleCount - titleCount} left</p></div>,
-//     className: 'title-holder',
-//     maxChars: 25,
-//     rows: 1,
-//     multiLine: false,
-//     rowsMax: null,
-//   },
-//   {
-//     name: 'description',
-//     label: 'What do you do with this list? e.g. The VIP list is used for people who have asked questions about our product and want to try our next update',
-//     formHeader: <div className='desc-details'><p className='desc-name'>Your list description(optional)</p><p>{maxDescCount - descCount} left</p></div>,
-//     className: 'desc-holder',
-//     maxChars: 130,
-//     rows: 2,
-//     multiLine: true,
-//     rowsMax: 4,
-//   }
-// ]
+export const formDataCreateNetworkListModal = (maxTitleCount, titleCount, maxDescCount, descCount) => [
+  {
+    name: 'title',
+    label: 'Type a list name, e.g. Top clients, freelancers, potential investors',
+    formHeader: <div className='title-details'><p className='list-name'>Your list title</p><p>{maxTitleCount - titleCount} left</p></div>,
+    className: 'title-holder',
+    maxChars: 25,
+    rows: 1,
+    multiLine: false,
+    rowsMax: null,
+  },
+  {
+    name: 'description',
+    label: 'What do you do with this list? e.g. The VIP list is used for people who have asked questions about our product and want to try our next update',
+    formHeader: <div className='desc-details'><p className='desc-name'>Your list description(optional)</p><p>{maxDescCount - descCount} left</p></div>,
+    className: 'desc-holder',
+    maxChars: 130,
+    rows: 2,
+    multiLine: true,
+    rowsMax: 4,
+  }
+]
+
 // used in ~/components/NetworkListForm.js
 export const formDataNetworkListForm = [
   {
