78478b6839bfd613c2515836718af310ceb5b073
raptorzhezuzninya <w22243vrh@gmail.com>
2017-05-04 17:01:59 +0200

    rendering 3 func that return all i need

--- src/components/sortContact/ContactCard.js
+++ src/components/sortContact/ContactCard.js
@@ -1,29 +1,62 @@
 import React, { PureComponent } from 'react'
 import Paper from 'material-ui/Paper'
 import './ContactCard.sass'
-
-const styles = {
-  paper: {
-    justifyContent: 'center',
-    flex: '0 0 100%',
-    height: 230
-  }
-}
+import { inlineContactCardStyles as styles } from '../../helpers/inlineStyles'
 
 class ContactCard extends PureComponent {
+  constructor(props) {
+    super(props)
+
+  }
+  renderContactDetails () {
+    const { contact } = this.props
+    return contact.map((c) => {
+      return (
+        <div className='details-holder'>
+          <div className='detail-item'>{c.firstName}</div>
+          <div className='detail-item'>{c.lastName}</div>
+          <div className='detail-item'>{c.companyName}</div>
+          <div className='detail-item'>{c.companyRole}</div>
+          <div className='detail-item'>{c.email}</div>
+        </div>
+      )
+    })
+  }
+
+  renderContactAvatar () {
+    const { contact } = this.props
+    return contact.map((c) => {
+      return (
+        <div className='avatar-holder'>
+          <img src={c.avatar} alt='user avatar'/>
+        </div>
+      )
+    })
+  }
+  renderContactEmail () {
+    const { contact } = this.props
+    return contact.map((c) => {
+      return (
+        <div className='email-header-holder'>
+          <p>{c.message}</p>
+        </div>
+      )
+    })
+  }
+
   render () {
+    const { contact } = this.props
     return (
       <div className='contact-card-holder'>
+
         <Paper style={styles.paper} zDepth={3}>
-          <div className='details-holder'>
 
+          <div className='paper-content-holder'>
+            {this.renderContactDetails()}
+            {this.renderContactAvatar()}
+            {this.renderContactEmail()}
           </div>
-          <div className='avatar-holder'>
 
-          </div>
-          <div className='email-header-holder'>
-
-          </div>
         </Paper>
       </div>
     )
