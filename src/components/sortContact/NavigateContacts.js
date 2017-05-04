1110bb17284c86adb403f9f3bf11eacc25c774ac
raptorzhezuzninya <w22243vrh@gmail.com>
2017-05-04 18:50:52 +0200

    create new comp for next and prev contact button

---
+++ src/components/sortContact/NavigateContacts.js
@@ -0,0 +1,45 @@
+import React, { PureComponent, PropTypes } from 'react'
+import IconButton from 'material-ui/IconButton'
+
+const styles = {
+  iconStyle: {
+    color: '#f2f6fa',
+    fontSize: 40
+  },
+  button: {
+    height: 100,
+    width: 100
+  },
+  hoveredStyle: {
+    backgroundColor:'rgba(41, 47, 54, 0.3)'
+  },
+}
+
+class NavigateContacts extends PureComponent {
+  render () {
+    return (
+      <div className='navigate-contacts-holder'>
+        <div className='button-contact-navigate'>
+          <IconButton
+            iconClassName='material-icons'
+            hoveredStyle={styles.hoveredStyle}
+            iconStyle={styles.iconStyle}
+            style={styles.button}>
+            navigate_next
+          </IconButton>
+        </div>
+        <div className='button-contact-navigatez'>
+          <IconButton
+            iconClassName='material-icons'
+            hoveredStyle={styles.hoveredStyle}
+            iconStyle={styles.iconStyle}
+            style={styles.button}>
+            navigate_before
+          </IconButton>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default NavigateContacts
