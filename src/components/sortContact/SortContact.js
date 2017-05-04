d9c3b7519fda8c41c50f20b177a94aaba2d95743
raptorzhezuzninya <w22243vrh@gmail.com>
2017-05-04 15:29:27 +0200

    connected sortcontacts add some logic to only get one contact that i can pass to gthe contact card so i can build it with data flowing in

--- src/components/sortContact/SortContact.js
+++ src/components/sortContact/SortContact.js
@@ -1,11 +1,28 @@
-import React, { PureComponent } from 'react'
+import React, { PureComponent, PropTypes } from 'react'
+import { connect } from 'react-redux'
 import ContactCard from './ContactCard'
 import ProgressIndicator from './ProgressIndicator'
 import PageTitle from '../PageTitle'
 import './SortContact.sass'
 
 class SortContact extends PureComponent {
+
+  getOneContact () {
+    const { contacts } = this.props
+    return contacts.filter((contact) => {
+      return contact.contactId === 1
+    })
+  }
+
+  returnSomeMarkup = (contact) => (
+    <div>
+      {contact.contactId}
+      {contact.firstName}
+    </div>
+  )
+
   render () {
+    const { contacts } = this.props
     return (
       <div className='sort-contact-wrapper'>
         <div className='progress-indicator-wrapper'>
@@ -17,11 +34,20 @@ class SortContact extends PureComponent {
             mode='determinate' />
         </div>
         <div className='contact-card-wrapper'>
-          <ContactCard />
+          <ContactCard contact={this.getOneContact().map((contact) => this.returnSomeMarkup(contact))}/>
+        </div>
+        <div className=''>
+          <pre><code>{JSON.stringify(this.getOneContact(), null, 4)}</code></pre>
+          { this.getOneContact().map((contact) => this.returnSomeMarkup(contact))}
         </div>
       </div>
     )
   }
 }
+const mapStateToProps = (state) => {
+  return {
+    contacts: state.contacts
+  }
+}
 
-export default SortContact
+export default connect(mapStateToProps)(SortContact)
