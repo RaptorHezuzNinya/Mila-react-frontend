b87066da76482a5f3bb28d699c2b9a8113aaf2c2
raptorzhezuzninya <w22243vrh@gmail.com>
2017-05-04 18:51:10 +0200

    import navigate contacts and proptypes

--- src/components/sortContact/SortContact.js
+++ src/components/sortContact/SortContact.js
@@ -3,21 +3,29 @@ import { connect } from 'react-redux'
 import ContactCard from './ContactCard'
 import ProgressIndicator from './ProgressIndicator'
 import PageTitle from '../PageTitle'
+import NavigateContacts from './NavigateContacts'
 import './SortContact.sass'
 
 class SortContact extends PureComponent {
+  static propTypes = {
+    contacts: PropTypes.array.isRequired
+  }
 
   getOneContact () {
     const { contacts } = this.props
+    console.log('logging CONTACTS', contacts)
     return contacts.filter((contact) => {
       return contact.contactId === 1
     })
   }
 
   render () {
+    console.log(this.getOneContact())
     // const { contacts } = this.props
+
     return (
       <div className='sort-contact-wrapper'>
+
         <div className='progress-indicator-wrapper'>
           <PageTitle
             titleClassName='sortcontact-title'
