28ac5f8c31c0b82e66858cf515295cc207fdd638
raptorzhezuzninya <w22243vrh@gmail.com>
2017-05-04 22:33:54 +0200

    removed completed prop from progress holder

--- src/components/sortContact/ProgressIndicator.js
+++ src/components/sortContact/ProgressIndicator.js
@@ -2,30 +2,29 @@ import React, { PureComponent } from 'react'
 import LinearProgress from 'material-ui/LinearProgress'
 import './ProgressIndicator.sass'
 
-const styles = {
-  progress: {
-    color: '#cccccc'
-  }
-}
+// const styles = {
+//   progress: {
+//     color: '#cccccc'
+//   }
+// }
 
 class ProgressIndicator extends PureComponent {
   constructor(props) {
     super(props)
     this.state = {
-      completed: 20
+
     }
   }
 
   render () {
-    const { completed } = this.state
-    const { mode } = this.props
+
+    const { mode, completedProgress } = this.props
     return (
       <div className='progress-indicator-holder'>
         <LinearProgress
           mode={mode}
-          value={completed}
-          style={styles.progress}
-          color='#5DD9B2'/>
+          value={completedProgress}
+          color='#5DD9B2' />
       </div>
     )
   }
