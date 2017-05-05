7f4d1d6cf39a304fb88ecc46dada835738a494ad
raptorzhezuzninya <w22243vrh@gmail.com>
2017-05-05 14:51:14 +0200

    create new component for networklistbuttons

---
+++ src/components/NetworkListButton.js
@@ -0,0 +1,13 @@
+import React, { PureComponent, PropTypes } from 'react'
+
+class NetworkListButton extends PureComponent {
+  render () {
+    return (
+      <div className='network-lists-holder'>
+
+      </div>
+    )
+  }
+}
+
+export default NetworkListButton
