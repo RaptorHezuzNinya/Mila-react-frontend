d4c6c1e56ccf4f62df52bf9e2915f8d280ba8a67
raptorzhezuzninya <w22243vrh@gmail.com>
2017-05-05 15:46:14 +0200

    add render func to render networkbuttons

--- src/components/NetworkListButton.js
+++ src/components/NetworkListButton.js
@@ -1,10 +1,26 @@
 import React, { PureComponent, PropTypes } from 'react'
+import FlatButton from 'material-ui/FlatButton'
 
 class NetworkListButton extends PureComponent {
+  static propTypes = {
+    networkLists: PropTypes.array.isRequired
+  }
+
+  renderNetworkLists () {
+    const { networkLists } = this.props
+    return networkLists.map((networkList) => {
+      return (
+        <div className='network-lists' key={networkList.id}>
+          <FlatButton label={networkList.title} />
+        </div>
+      )
+    })
+  }
+
   render () {
     return (
       <div className='network-lists-holder'>
-
+        {this.renderNetworkLists()}
       </div>
     )
   }
