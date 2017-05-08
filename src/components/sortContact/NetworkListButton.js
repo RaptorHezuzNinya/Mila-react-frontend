dec99320c5f38f72a12279ed7e02eef5335f3927
raptorzhezuzninya <w22243vrh@gmail.com>
2017-05-09 00:32:45 +0200

    added snackbar with 2 needed methods to networklistbuton

--- src/components/sortContact/NetworkListButton.js
+++ src/components/sortContact/NetworkListButton.js
@@ -2,25 +2,48 @@ import React, { PureComponent, PropTypes } from 'react'
 import { connect } from 'react-redux'
 import { addContactToNetworklist } from '../../actions/networklists'
 import { addNetworkListToContact } from '../../actions/contacts'
+import Snackbar from 'material-ui/Snackbar'
 import FlatButton from 'material-ui/FlatButton'
 import ListIcon from 'material-ui/svg-icons/action/list'
 import './NetworkListButton.sass'
 
 class NetworkListButton extends PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = {
+      snackOpen: false,
+
+    }
+  }
   static propTypes = {
     networkLists: PropTypes.array.isRequired,
     oneContact: PropTypes.array.isRequired
   }
 
+  handleRequestClose = () => {
+    this.setState({
+      snackOpen: false,
+    })
+  }
+  
+  handleRequestOpen = () => {
+    this.setState({
+      snackOpen: true,
+    });
+  };
+
+
+
   handleNetworkButtonClick (networkListId) {
     const { oneContact } = this.props
     const theOneContactId = oneContact[0].id
+    this.handleRequestOpen()
     this.props.addContactToNetworklist(theOneContactId, networkListId)
     this.props.addNetworkListToContact(networkListId, theOneContactId)
   }
 
   renderNetworkLists () {
-    const { networkLists } = this.props
+    const { networkLists, } = this.props
     return networkLists.map((networkList) => {
       return (
         <div className='network-lists' key={networkList.id}>
