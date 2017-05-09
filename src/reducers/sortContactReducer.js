91940fec8b766e2e76f80d37f9a97789780a9569
raptorzhezuzninya <w22243vrh@gmail.com>
2017-05-10 00:52:49 +0200

    create new sortContactReducer

---
+++ src/reducers/sortContactReducer.js
@@ -0,0 +1,15 @@
+
+initialState = {
+
+}
+
+export default function sortContactReducer(state = initialState, { type, payload } = {}) {
+  switch (type) {
+    // case expression:
+
+
+    default:
+      return state
+
+  }
+}
