33cc0084555de9cd84d6a288f9521e38544cd658
raptorzhezuzninya <w22243vrh@gmail.com>
2017-05-10 13:45:13 +0200

    in sortcontact reducer added case

--- src/reducers/sortContactReducer.js
+++ src/reducers/sortContactReducer.js
@@ -1,11 +1,17 @@
+import { ADD_CONTACT_TO_NETWORKLIST } from '../actions/networklists'
 
 const initialState = {
-
+  addedContactIds: []
 }
 
 export default function sortContactReducer(state = initialState, { type, payload } = {}) {
   switch (type) {
-    // case expression:
+    case ADD_CONTACT_TO_NETWORKLIST :
+      if (state.addedContactIds.includes(payload.contactId)) {
+        return state
+      }
+      return { ...state, addedContactIds: [...state.addedContactIds, payload.contactId] }
+
 
 
     default:
