061faafa66ac5ac5f504416d59a4b385351c2b14
raptorzhezuzninya <w22243vrh@gmail.com>
2017-05-10 14:19:22 +0200

    classname change inlinestyle refactor

--- src/components/sortContact/NavigateContacts.js
+++ src/components/sortContact/NavigateContacts.js
@@ -3,19 +3,25 @@ import IconButton from 'material-ui/IconButton'
 import './NavigateContacts.sass'
 
 const styles = {
-  iconStyle: {
+  iconStyleNext: {
     color: '#f2f6fa',
     fontSize: 40,
     display: 'flex',
-    justifyContent: 'center'
+    justifyContent: 'flex-end'
+  },
+  iconStylePrev: {
+    color: '#f2f6fa',
+    fontSize: 40,
+    display: 'flex',
+    justifyContent: 'flex-start'
   },
   button: {
     height: 100,
-    width: 50
-  },
-  hoveredStyle: {
-    backgroundColor:'rgba(41, 47, 54, 0.3)'
-  },
+    width: 100
+  }
+  // hoveredStyle: {
+  //   backgroundColor:'rgba(41, 47, 54, 0.3)'
+  // },
 }
 
 class NavigateContacts extends PureComponent {
@@ -26,22 +32,22 @@ class NavigateContacts extends PureComponent {
   render () {
     return (
       <div className='navigate-contacts-holder'>
-        <div className='button-contact-navigatez'>
+        <div className='contact-navigate-prev'>
           <IconButton
             onClick={this.props.handlePrevContact}
             iconClassName='material-icons'
             hoveredStyle={styles.hoveredStyle}
-            iconStyle={styles.iconStyle}
+            iconStyle={styles.iconStylePrev}
             style={styles.button}>
             navigate_before
           </IconButton>
         </div>
-        <div className='button-contact-navigate'>
+        <div className='contact-navigate-next'>
           <IconButton
             onClick={this.props.handleNextContact}
             iconClassName='material-icons'
             hoveredStyle={styles.hoveredStyle}
-            iconStyle={styles.iconStyle}
+            iconStyle={styles.iconStyleNext}
             style={styles.button}>
             navigate_next
           </IconButton>
