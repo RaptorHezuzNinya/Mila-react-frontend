93edbc8364b6842ebefecd92998f365830551713
raptorzhezuzninya <w22243vrh@gmail.com>
2017-05-29 20:12:25 +0200

    add contactformdetails to MSTP

--- src/components/sortContact/NetworkListButton.js
+++ src/components/sortContact/NetworkListButton.js
@@ -127,7 +127,8 @@ class NetworkListButton extends PureComponent {
 
 const mapStateToProps = (state) => {
   return {
-    networkLists: state.networkLists
+    networkLists: state.networkLists,
+    contactDetailsForm: state.form.contactDetailsForm
   }
 }
 
