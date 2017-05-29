8805ab8768921a7f9b3589e6694ea5f499eb6647
raptorzhezuzninya <w22243vrh@gmail.com>
2017-05-29 23:53:49 +0200

    create deletecard comp to show if user deletes a contact

---
+++ src/components/sortContact/DeleteCard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const DeleteCard = (props) => {
+  return (
+    <div className='delete-card-holder'>
+      <h2>Ok!</h2>
+      <p>This contact will be kept out of my records.</p>
+    </div>
+  )
+}
+
+export default DeleteCard
