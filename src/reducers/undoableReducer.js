f1af78fe90f7f89ad2ac376d3746ac4f58a1cb44
raptorzhezuzninya <w22243vrh@gmail.com>
2017-06-01 21:45:37 +0200

    refactor undoable reducer to work with 1 in present rest in futuet

--- src/reducers/undoableReducer.js
+++ src/reducers/undoableReducer.js
@@ -1,36 +1,48 @@
-import { UNDO, REDO } from './actions/undoable'
+import { UNDO, REDO } from '../actions/undoable'
 
-const initialState = {
-  past: [],
-  present: [],
-  future: [],
-}
-const expression = null
+export function undoable(reducer) {
 
-export const undoable = (state = initialState, {type, action} = {}) => {
-  const { past, present, future } = state
+  const initialState = {
+    past: [],
+    present: reducer(undefined, {}),
+    future: [],
+  }
 
-  switch (type) {
-    case UNDO :
-      const previous = past[past.length - 1]
-      const newPast = past.slice(0, past.length - 1)
-      return {
-        past: newPast,
-        present: previous,
-        future: [present, ...future]
-      }
+  return (state = initialState, action) => {
+    const { past, present, future } = state
 
-    case REDO :
-      const next = future[0]
-      const newFuture = future.slice(1)
-      return {
-        past: [...past, present],
-        present: next,
-        future: newFuture
-      }
+    switch (action.type) {
+      case UNDO :
+        const previous = past[past.length - 1]
+        const newPast = past.slice(0, past.length - 1)
+        return {
+          past: newPast,
+          present: previous,
+          future: [present, ...future]
+        }
 
-    default:
-      return state
+      case REDO :
+        const next = future[0]
+        const newFuture = future.slice(1)
+        return {
+          past: [...past, present],
+          present: next,
+          future: newFuture
+        }
 
+      default:
+        const newPresent = reducer(present, action)
+        if (present === newPresent) {
+          return state
+        }
+        const neededPresent = newPresent.contacts.slice(0, 1)
+        const neededFuture = newPresent.contacts.slice(1)
+        console.log('newPresent', newPresent)
+        return {
+          past: [...past, present],
+          present: neededPresent,
+          future: neededFuture
+        }
+    }
   }
 }
