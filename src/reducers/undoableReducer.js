88d30695237fa53200dbf6a33c4a596a351fb5d8
raptorzhezuzninya <w22243vrh@gmail.com>
2017-06-01 19:58:42 +0200

    add undo and redo case

--- src/reducers/undoableReducer.js
+++ src/reducers/undoableReducer.js
@@ -1,4 +1,4 @@
-// import { UNDO, REDO}
+import { UNDO, REDO } from './actions/undoable'
 
 const initialState = {
   past: [],
@@ -9,9 +9,25 @@ const expression = null
 
 export const undoable = (state = initialState, {type, action} = {}) => {
   const { past, present, future } = state
-  switch (type) {
-    case expression:
 
+  switch (type) {
+    case UNDO :
+      const previous = past[past.length - 1]
+      const newPast = past.slice(0, past.length - 1)
+      return {
+        past: newPast,
+        present: previous,
+        future: [present, ...future]
+      }
+
+    case REDO :
+      const next = future[0]
+      const newFuture = future.slice(1)
+      return {
+        past: [...past, present],
+        present: next,
+        future: newFuture
+      }
 
     default:
       return state
