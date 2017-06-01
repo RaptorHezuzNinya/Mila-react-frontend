e363de93e5a5e8cf5c510f318d23e61e071490ec
raptorzhezuzninya <w22243vrh@gmail.com>
2017-06-01 21:31:29 +0200

    add request and receive actions to laoding reducer

--- src/reducers/loadingReducer.js
+++ src/reducers/loadingReducer.js
@@ -1,10 +1,15 @@
 import { LOADING_CONTACTS, LOADING_CONTACTS_DONE } from '../actions/loading'
+import { REQUEST_CONTACTS, RECEIVE_CONTACTS } from '../actions/contacts'
 
 export default function loadingReducer(state = false, {type} = {}) {
   switch (type) {
+
     case LOADING_CONTACTS :
+    case REQUEST_CONTACTS :
       return true
+
     case LOADING_CONTACTS_DONE :
+    case RECEIVE_CONTACTS :
       return false
 
     default:
