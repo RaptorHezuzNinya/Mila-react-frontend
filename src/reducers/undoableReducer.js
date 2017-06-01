86cfadc0f5e3951db2d6033a0032379cba4230dd
raptorzhezuzninya <w22243vrh@gmail.com>
2017-06-01 19:09:57 +0200

    edits to undoable

--- src/reducers/undoableReducer.js
+++ src/reducers/undoableReducer.js
@@ -1,10 +1,14 @@
+// import { UNDO, REDO}
+
 const initialState = {
   past: [],
   present: [],
   future: [],
 }
 const expression = null
+
 export const undoable = (state = initialState, {type, action} = {}) => {
+  const { past, present, future } = state
   switch (type) {
     case expression:
 
