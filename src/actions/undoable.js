7b9bece5a01ab25410bf0eac824448ad37927fbd
raptorzhezuzninya <w22243vrh@gmail.com>
2017-06-01 19:58:35 +0200

    create undoable actions

---
+++ src/actions/undoable.js
@@ -0,0 +1,13 @@
+export const UNDO = 'UNDO'
+export const undo = () => {
+  return {
+    type: UNDO
+  }
+}
+
+export const REDO = 'REDO'
+export const undo = () => {
+  return {
+    type: REDO
+  }
+}
