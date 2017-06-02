a23e116af0dd758e958c196f8ad9edc9c52871a5
raptorzhezuzninya <w22243vrh@gmail.com>
2017-06-02 12:52:50 +0200

    new file for all actions related to sortcontacts

---
+++ src/actions/sortContacts.js
@@ -0,0 +1,25 @@
+export const REQUEST_CONTACTS = 'REQUEST_CONTACTS'
+const requestContacts = () => {
+  return {
+    type: REQUEST_CONTACTS,
+  }
+}
+
+export const RECEIVE_CONTACTS = 'RECEIVE_CONTACTS'
+const receiveContacts = (json) => {
+  return {
+    type: RECEIVE_CONTACTS,
+    payload: json
+  }
+}
+
+export const FETCH_CONTACTS = 'FETCH_CONTACTS'
+export const fetchContacts = () => {
+  return (dispatch) => {
+    dispatch(requestContacts())
+    return axios.get(`${rootUrl}contacts?_page=1&_limit=25`)
+    .then((response) => response.data)
+    .catch((error) => console.log(error))
+    .then((json) => dispatch(receiveContacts(json)))
+  }
+}
