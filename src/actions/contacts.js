bd25e7ca206ce3d604276a683d97d1484f5bf744
raptorzhezuzninya <w22243vrh@gmail.com>
2017-06-02 13:31:48 +0200

    moved sortcontact related actions to sortcotnacts actions moved axios import :

--- src/actions/contacts.js
+++ src/actions/contacts.js
@@ -1,7 +1,3 @@
-import fetch from 'isomorphic-fetch'
-import axios from 'axios'
-
-const rootUrl = 'http://localhost:3000/'
 export const UPDATE_CONTACT = 'UPDATE_CONTACT'
 export const updateContact = (contactFields, contactId) => {
   return {
@@ -29,32 +25,5 @@ export const addNetworkListToContact = (networkListId, contactId) => {
       contactId,
       networkListId
     }
-  }
-}
-
-
-export const FETCH_CONTACTS = 'FETCH_CONTACTS'
-export const fetchContacts = () => {
-  return (dispatch) => {
-    dispatch(requestContacts())
-    return axios.get(`${rootUrl}contacts?_page=1&_limit=25`)
-      .then((response) => response.data)
-      .catch((error) => console.log(error))
-      .then((json) => dispatch(receiveContacts(json)))
-  }
-}
-
-export const REQUEST_CONTACTS = 'REQUEST_CONTACTS'
-const requestContacts = () => {
-  return {
-    type: REQUEST_CONTACTS,
-  }
-}
-
-export const RECEIVE_CONTACTS = 'RECEIVE_CONTACTS'
-const receiveContacts = (json) => {
-  return {
-    type: RECEIVE_CONTACTS,
-    payload: json
   }
 }
--- src/actions/sortContacts.js
+++ src/actions/sortContacts.js
@@ -1,3 +1,6 @@
+import axios from 'axios'
+const rootUrl = 'http://localhost:3000/'
+
 export const REQUEST_CONTACTS = 'REQUEST_CONTACTS'
 const requestContacts = () => {
   return {
