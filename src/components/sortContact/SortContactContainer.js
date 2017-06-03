6d28e5fda45cb698816c77ab7f7a9aeb2bd45142
raptorzhezuzninya <w22243vrh@gmail.com>
2017-06-03 02:24:45 +0200

    add prop-types package since react proptypes is deprecated since 15+

--- src/components/sortContact/SortContactContainer.js
+++ src/components/sortContact/SortContactContainer.js
@@ -1,4 +1,5 @@
-import React, { PureComponent, PropTypes } from 'react';
+import React, { PureComponent } from 'react';
+import { PropTypes } from 'prop-types';
 import { connect } from 'react-redux';
 import { submit } from 'redux-form';
 import { updateContact, deleteContact } from '../../actions/contacts';
