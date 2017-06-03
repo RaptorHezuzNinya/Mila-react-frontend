a25af3664a125002290459e2f58220203dfed601
raptorzhezuzninya <w22243vrh@gmail.com>
2017-06-04 00:39:39 +0200

    refactor to arrow func reducer

--- src/reducers/undoableReducer.js
+++ src/reducers/undoableReducer.js
@@ -1,6 +1,6 @@
 import { UNDO, REDO } from '../actions/undoable';
 
-export function undoable(reducer) {
+export const undoable = reducer => {
   const initialState = {
     past: [],
     present: reducer(undefined, {}),
@@ -12,40 +12,40 @@ export function undoable(reducer) {
     const { past, present, future } = state;
 
     switch (action.type) {
-      case UNDO:
-        const previous = past[past.length - 1];
-        const newPast = past.slice(0, past.length - 1);
-        return {
-          past: newPast,
-          present: previous,
-          future: [present, ...future],
-        };
-
-      case REDO:
-        const next = future[0];
-        const newFuture = future.slice(1);
-        const sortingState = state.sortingData;
-        return {
-          past: [...past, present],
-          present: next,
-          future: newFuture,
-          sortingData: { ...sortingState },
-        };
+      // case UNDO:
+      //   const previous = past[past.length - 1];
+      //   const newPast = past.slice(0, past.length - 1);
+      //   return {
+      //     past: newPast,
+      //     present: previous,
+      //     future: [present, ...future],
+      //   };
+      //
+      // case REDO:
+      //   const next = future[0];
+      //   const newFuture = future.slice(1);
+      //   const sortingState = state.sortingData;
+      //   return {
+      //     past: [...past, present],
+      //     present: next,
+      //     future: newFuture,
+      //     sortingData: { ...sortingState },
+      //   };
 
       default:
         const newPresent = reducer(present, action);
-        // console.log('present', present, 'newPresent', newPresent);
+        console.log('present', present, 'newPresent', newPresent);
         if (present === newPresent) {
           return state;
         }
         const neededPresent = newPresent.totalSortContacts.slice(0, 1);
         const neededFuture = newPresent.totalSortContacts.slice(1);
         return {
-          past: [...past],
-          present: {neededPresent[0], ...newPresent},
+          past: [...past, present],
+          present: neededPresent[0],
           future: neededFuture,
           sortingData: { ...newPresent },
         };
     }
   };
-}
+};
