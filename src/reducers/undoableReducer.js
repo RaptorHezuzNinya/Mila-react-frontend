8dcb5d23cbe3b8283815e97cf7a1c2a4e47c2c1e
raptorzhezuzninya <w22243vrh@gmail.com>
2017-06-03 02:19:14 +0200

    prettier

--- src/reducers/undoableReducer.js
+++ src/reducers/undoableReducer.js
@@ -1,53 +1,51 @@
-import { UNDO, REDO } from '../actions/undoable'
+import { UNDO, REDO } from '../actions/undoable';
 
 export function undoable(reducer) {
-
   const initialState = {
     past: [],
     present: reducer(undefined, {}),
     future: [],
-  }
+  };
 
   return (state = initialState, action) => {
-    const { past, present, future } = state
+    const { past, present, future } = state;
 
     switch (action.type) {
-      case UNDO :
-        const previous = past[past.length - 1]
-        const newPast = past.slice(0, past.length - 1)
+      case UNDO:
+        const previous = past[past.length - 1];
+        const newPast = past.slice(0, past.length - 1);
         return {
           past: newPast,
           present: previous,
-          future: [present, ...future]
-        }
+          future: [present, ...future],
+        };
 
-      case REDO :
-        const next = future[0]
-        const newFuture = future.slice(1)
+      case REDO:
+        const next = future[0];
+        const newFuture = future.slice(1);
         return {
           past: [...past, present],
           present: next,
-          future: newFuture
-        }
+          future: newFuture,
+        };
 
       default:
-        const newPresent = reducer(present, action)
+        const newPresent = reducer(present, action);
 
-        console.log('present:', present, 'newPresent:', newPresent)
-        console.log('state', state)
+        console.log('present:', present, 'newPresent:', newPresent);
+        console.log('state', state);
 
         if (present === newPresent) {
-          return state
+          return state;
         }
-        const neededPresent = newPresent.contacts.slice(0, 1)
-        const neededFuture = newPresent.contacts.slice(1)
+        const neededPresent = newPresent.contacts.slice(0, 1);
+        const neededFuture = newPresent.contacts.slice(1);
         // console.log('newPresent', newPresent)
         return {
           past: [...past, present],
           present: neededPresent[0],
           future: neededFuture,
-
-        }
+        };
     }
-  }
+  };
 }
