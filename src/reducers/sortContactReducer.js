d5ac7e6c5bfb1623b141e41dc44afe18a1468465
raptorzhezuzninya <w22243vrh@gmail.com>
2017-06-05 23:48:50 +0200

    add case in undoable reducer

--- src/reducers/sortContactReducer.js
+++ src/reducers/sortContactReducer.js
@@ -1,51 +1,50 @@
 import { ADD_CONTACT_TO_NETWORKLIST } from '../actions/networklists';
 import { REQUEST_CONTACTS, RECEIVE_CONTACTS } from '../actions/sortContacts';
+import { ADD_NETWORKLIST_TO_CONTACT } from '../actions/contacts';
 import { undoable } from './undoableReducer';
+import store from '../store';
 
 const initialState = {
   totalSortContacts: [],
   addedContactIds: [],
 };
 
-// const initialState2 = {
-//   totalSortContacts2: [],
-//   addedContactIds2: [],
-// };
-
-// const soloContact = (state = initialState2, { type, payload } = {}) => {
-//   switch (type) {
-//     case ADD_CONTACT_TO_NETWORKLIST:
-//       if (state.addedContactIds.includes(payload.contactId)) {
-//         return state;
-//       }
-//       return {
-//         addedContactIds: [...state.addedContactIds, payload.contactId],
-//       }
-//       return;
-//     default:
-//       return state;
-//   }
-// };
-
 const sortContactReducer = (state = initialState, { type, payload } = {}) => {
   switch (type) {
-    case ADD_CONTACT_TO_NETWORKLIST:
-      console.log('STATE in reducer', state);
-      // if (state.addedContactIds.includes(payload.contactId)) {
-      //   return state;
-      // }
-      return {
-        ...state,
-        addedContactIds: [...state.addedContactIds, payload.contactId],
-      };
-    // case REQUEST_CONTACTS :
-    //   return {...state}
+    // case ADD_CONTACT_TO_NETWORKLIST:
+    //   const customState = store.getState();
+    //   console.log(customState, 'customState');
+    //   const sortContactState = customState.sortContact;
+    //
+    //   return {
+    //     ...sortContactState,
+    //     sortingData: {
+    //       addedContactIds: [
+    //         ...sortContactState.sortingData.addedContactIds,
+    //         payload.contactId,
+    //       ],
+    //     },
+    //   };
+    // case REQUEST_CONTACTS:
+    //   return { ...state };
 
+    case ADD_NETWORKLIST_TO_CONTACT:
+      console.log(
+        'state adsfasdfasdfasdfasdsortDFDSFSDFDSSDFDSFDSContact',
+        state
+      );
+      if (state.id === payload.contactId) {
+        let newIdArr = state.networkListIds.slice();
+        newIdArr.splice(0, 0, payload.networkListId);
+        return { ...state, networkListIds: newIdArr };
+      }
+      return state;
 
     case RECEIVE_CONTACTS:
       return { ...state, totalSortContacts: payload };
 
     default:
+      console.log('IN SORTCONTACT DEFAULT');
       return state;
   }
 };
--- src/reducers/undoableReducer.js
+++ src/reducers/undoableReducer.js
@@ -1,43 +1,73 @@
 import { UNDO, REDO } from '../actions/undoable';
 
-export const undoable = reducer => {
+import { ADD_CONTACT_TO_NETWORKLIST } from '../actions/networklists';
+export const undoable = sortContactReducer => {
+  const sortContactReducerConst = sortContactReducer(undefined, {});
   const initialState = {
     past: [],
-    present: reducer(undefined, {}),
+    present: sortContactReducer(undefined, {}),
     future: [],
-    sortingData: reducer(undefined, {}),
+    sortingData: sortContactReducerConst,
   };
 
   return (state = initialState, action) => {
     const { past, present, future } = state;
 
+    const sortingState = state.sortingData;
     switch (action.type) {
-      // case UNDO:
-      //   const previous = past[past.length - 1];
-      //   const newPast = past.slice(0, past.length - 1);
-      //   return {
-      //     past: newPast,
-      //     present: previous,
-      //     future: [present, ...future],
-      //   };
-      //
-      // case REDO:
-      //   const next = future[0];
-      //   const newFuture = future.slice(1);
-      //   const sortingState = state.sortingData;
-      //   return {
-      //     past: [...past, present],
-      //     present: next,
-      //     future: newFuture,
-      //     sortingData: { ...sortingState },
-      //   };
+      case UNDO:
+        const previous = past[past.length - 1];
+        const newPast = past.slice(0, past.length - 1);
+        return {
+          past: newPast,
+          present: previous,
+          future: [present, ...future],
+          sortingData: { ...sortingState },
+        };
 
-      default:
-        const newPresent = reducer(present, action);
-        console.log('present', present, 'newPresent', newPresent);
-        if (present === newPresent) {
+      case REDO:
+        const next = future[0];
+        const newFuture = future.slice(1);
+
+        return {
+          past: [...past, present],
+          present: next,
+          future: newFuture,
+          sortingData: { ...sortingState },
+        };
+
+      case ADD_CONTACT_TO_NETWORKLIST:
+        if (
+          state.sortingData.addedContactIds.includes(action.payload.contactId)
+        ) {
           return state;
         }
+        return {
+          ...state,
+          sortingData: {
+            ...state.sortingData,
+            addedContactIds: [
+              ...state.sortingData.addedContactIds,
+              action.payload.contactId,
+            ],
+          },
+        };
+
+      default:
+        const newPresent = sortContactReducer(present, action);
+        console.log(
+          'present',
+          present,
+          'newPresent',
+          newPresent,
+          'state',
+          state
+        );
+        if (present === newPresent) {
+          console.log('ITS EQUAL');
+          return state;
+        }
+        console.log('YOLOSWAG');
         const neededPresent = newPresent.totalSortContacts.slice(0, 1);
         const neededFuture = newPresent.totalSortContacts.slice(1);
         return {
