da54d11548c9a54f54cd5549c7e81c8e2ef5b8b2
raptorzhezuzninya <w22243vrh@gmail.com>
2017-06-06 17:40:14 +0200

    prettier reformat

--- src/App.js
+++ src/App.js
@@ -1,51 +1,52 @@
-import React, { Component } from 'react'
-import classNames from 'classNames'
-import Navigation from './components/Navigation'
-import MenuDrawer from './components/MenuDrawer'
-import ModalRoot from './components/modals/ModalRoot'
-import LoadingContacts from './components/LoadingContacts'
+import React, { Component } from 'react';
+import classNames from 'classNames';
+import Navigation from './components/Navigation';
+import MenuDrawer from './components/MenuDrawer';
+import ModalRoot from './components/modals/ModalRoot';
+import LoadingContacts from './components/LoadingContacts';
 // Material UI Components
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import muiTheme from './assets/styles/theme'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import muiTheme from './assets/styles/theme';
 // styles for app layout
-import './assets/styles/base/layout.sass'
-
+import './assets/styles/base/layout.sass';
 
 class App extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      openDrawer: false
-    }
-    this.handleOpenMenuDrawer = this.handleOpenMenuDrawer.bind(this)
+      openDrawer: false,
+    };
+    this.handleOpenMenuDrawer = this.handleOpenMenuDrawer.bind(this);
   }
 
   static childContextTypes = {
     muiTheme: React.PropTypes.object.isRequired,
-  }
+  };
 
   getChildContext() {
-    return { muiTheme }
+    return { muiTheme };
   }
 
-  handleOpenMenuDrawer () {
+  handleOpenMenuDrawer() {
     this.setState({
-      openDrawer: !this.state.openDrawer
-    })
+      openDrawer: !this.state.openDrawer,
+    });
   }
-
 
   render() {
-    const { openDrawer } = this.state
+    const { openDrawer } = this.state;
     const contentClass = classNames({
-      'content': true,
+      content: true,
       'content-drawer-open-desk': openDrawer,
-    })
+    });
     return (
       <MuiThemeProvider muiTheme={muiTheme}>
-        <div className='app'>
-          <Navigation handleOpenMenuDrawer={this.handleOpenMenuDrawer}/>
-          <MenuDrawer handleOpenMenuDrawer={this.handleOpenMenuDrawer} openDrawer={this.state.openDrawer}/>
+        <div className="app">
+          <Navigation handleOpenMenuDrawer={this.handleOpenMenuDrawer} />
+          <MenuDrawer
+            handleOpenMenuDrawer={this.handleOpenMenuDrawer}
+            openDrawer={this.state.openDrawer}
+          />
           <ModalRoot />
           <LoadingContacts />
           <div className={contentClass}>
@@ -53,8 +54,8 @@ class App extends Component {
           </div>
         </div>
       </MuiThemeProvider>
-    )
+    );
   }
 }
 
-export default App
+export default App;
