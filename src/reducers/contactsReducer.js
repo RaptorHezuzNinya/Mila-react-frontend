221934f2fc27edb6466a2064ced4365eb8318d3f
raptorzhezuzninya <w22243vrh@gmail.com>
2017-06-07 13:47:34 +0200

    moved update contact to sortContact reducer

--- src/reducers/contactsReducer.js
+++ src/reducers/contactsReducer.js
@@ -1,20 +1,9 @@
 import { UPDATE_CONTACT, DELETE_CONTACT } from '../actions/contacts';
 
-const initialState = {
-  totalContacts: [],
-};
+const initialState = {};
 
 const contactsReducer = (state = initialState, { type, payload } = {}) => {
   switch (type) {
-    case UPDATE_CONTACT:
-      return state.map(contact => {
-        if (contact.id === payload.contactId) {
-          const updatedContactAttributes = payload.contactFields;
-          return { ...contact, ...updatedContactAttributes };
-        }
-        return contact;
-      });
-
     case DELETE_CONTACT:
       const mappedIds = payload.map(contact => {
         return contact.id;
