8711855b0e410cb5133ec3836036e6de33e79d8a
raptorzhezuzninya <w22243vrh@gmail.com>
2017-06-07 13:09:09 +0200

    prettier reformat

--- src/components/sortContact/ContactCard.js
+++ src/components/sortContact/ContactCard.js
@@ -1,65 +1,66 @@
-import React, { PureComponent, PropTypes } from 'react'
-import { connect } from 'react-redux'
-import ContactDetails from './ContactDetails'
-import Paper from 'material-ui/Paper'
-import './ContactCard.sass'
-import { inlineContactCardStyles as styles } from '../../helpers/inlineStyles'
+import React, { PureComponent, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import ContactDetails from './ContactDetails';
+import Paper from 'material-ui/Paper';
+import './ContactCard.sass';
+import { inlineContactCardStyles as styles } from '../../helpers/inlineStyles';
 
 class ContactCard extends PureComponent {
   static propTypes = {
-    onSubmit: PropTypes.func.isRequired
-  }
+    onSubmit: PropTypes.func.isRequired,
+  };
 
-  renderContactAvatar () {
-    const { currentContact } = this.props
-      if (!currentContact) return null
-      return (
-        <div className='avatar-holder'>
-          <img src={currentContact.avatar} alt='user avatar'/>
-        </div>
-      )
-
+  renderContactAvatar() {
+    const { currentContact } = this.props;
+    if (!currentContact) return null;
+    return (
+      <div className="avatar-holder">
+        <img src={currentContact.avatar} alt="user avatar" />
+      </div>
+    );
   }
 
   renderContactEmail() {
-    const { currentContact } = this.props
-    if (!currentContact) return null
+    const { currentContact } = this.props;
+    if (!currentContact) return null;
     return (
-      <div className='email-adress'>
+      <div className="email-adress">
         <p>{currentContact.email}</p>
       </div>
-    )
+    );
   }
 
-  renderEmailHeader () {
-    const { currentContact } = this.props
-    if (!currentContact) return null
+  renderEmailHeader() {
+    const { currentContact } = this.props;
+    if (!currentContact) return null;
     return (
-      <div className='email-header-holder'>
+      <div className="email-header-holder">
         <p>{currentContact.message}</p>
       </div>
-    )
+    );
   }
 
-  render () {
-
+  render() {
     return (
-      <div className='contact-card-holder'>
+      <div className="contact-card-holder">
         <Paper style={styles.paper} zDepth={2}>
-          <div className='paper-content-holder'>
-            <ContactDetails onSubmit={this.props.onSubmit} currentContact={this.props.currentContact}/>
+          <div className="paper-content-holder">
+            <ContactDetails
+              onSubmit={this.props.onSubmit}
+              currentContact={this.props.currentContact}
+            />
             {this.renderContactAvatar()}
             {this.renderContactEmail()}
             {this.renderEmailHeader()}
           </div>
         </Paper>
       </div>
-    )
+    );
   }
 }
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
-    currentContact: state.sortContact.present
-  }
-}
-export default connect(mapStateToProps)(ContactCard)
+    currentContact: state.sortContact.present,
+  };
+};
+export default connect(mapStateToProps)(ContactCard);
