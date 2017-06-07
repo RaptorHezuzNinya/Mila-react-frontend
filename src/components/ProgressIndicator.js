475036c9688645ecfaf1a95b492c313ad9a10cb6
raptorzhezuzninya <w22243vrh@gmail.com>
2017-06-07 14:34:52 +0200

    refactor progress indicator to be func comp

--- src/components/ProgressIndicator.js
+++ src/components/ProgressIndicator.js
@@ -1,25 +1,20 @@
-import React, { PureComponent, PropTypes } from 'react'
-import LinearProgress from 'material-ui/LinearProgress'
+import React, { PureComponent } from 'react';
+import { PropTypes } from 'prop-types';
+import LinearProgress from 'material-ui/LinearProgress';
 // sass for this comp can be found where ever it is rendered
-class ProgressIndicator extends PureComponent {
-  static propTypes = {
+
+const ProgressIndicator = ({ mode, completedProgress, color, holderClass }) => {
+  return (
+    <div className={holderClass}>
+      <LinearProgress mode={mode} value={completedProgress} color={color} />
+    </div>
+  );
+  ProgressIndicator.propTypes = {
     mode: PropTypes.string.isRequired,
     completedProgress: PropTypes.number.isRequired,
     color: PropTypes.string.isRequired,
-    holderClass: PropTypes.string.isRequired
-  }
+    holderClass: PropTypes.string.isRequired,
+  };
+};
 
-  render () {
-    const { mode, completedProgress, color, holderClass } = this.props
-    return (
-      <div className={holderClass}>
-        <LinearProgress
-          mode={mode}
-          value={completedProgress}
-          color={color} />
-      </div>
-    )
-  }
-}
-
-export default ProgressIndicator
+export default ProgressIndicator;
