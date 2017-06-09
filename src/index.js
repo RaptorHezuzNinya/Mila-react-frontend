17ddc06e06d1bf0d66072cec9b34748923d6d7e8
raptorzhezuzninya <w22243vrh@gmail.com>
2017-06-09 15:00:28 +0200

    prettier reformat

--- src/index.js
+++ src/index.js
@@ -1,39 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import { Router, Route, IndexRoute } from 'react-router'
-import store, { history } from './store'
-import injectTapEventPlugin from 'react-tap-event-plugin'
-import routes from './middleware/routes'
-import App from './App'
-import Onboarding from './components/onboarding/Onboarding'
-import SortContactContainer from './components/sortContact/SortContactContainer'
-import Network from './components/myNetwork/Network'
-import SettingsAccount from './components/settingsAccount/SettingsAccount'
-import SettingsList from './components/settingsList/SettingsList'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { Router, Route, IndexRoute } from 'react-router';
+import store, { history } from './store';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import routes from './middleware/routes';
+import App from './App';
+import Onboarding from './components/onboarding/Onboarding';
+import SortContactContainer
+  from './components/sortContact/SortContactContainer';
+import Network from './components/myNetwork/Network';
+import SettingsAccount from './components/settingsAccount/SettingsAccount';
+import SettingsList from './components/settingsList/SettingsList';
+import './assets/styles/main.sass';
+import 'normalize.css';
+import './assets/styles/base/basestyles.sass';
 
-import LoadingContacts from './components/LoadingContacts'
-import './assets/styles/main.sass'
-import 'normalize.css'
-import './assets/styles/base/basestyles.sass'
-
-injectTapEventPlugin()
+injectTapEventPlugin();
 
 ReactDOM.render(
   <Provider store={store}>
     <Router history={history}>
       <Route path="/" component={App}>
-      <IndexRoute component={Onboarding} />
+        <IndexRoute component={Onboarding} />
         <Route path={routes.onboardingPath} component={Onboarding} />
         <Route path={routes.sortContactPath} component={SortContactContainer} />
         <Route path={routes.myNetworkPath} component={Network} />
         <Route path={routes.settingsAccountPath} component={SettingsAccount} />
         <Route path={routes.settingsListPath} component={SettingsList} />
-
-        <Route path={routes.loadingContactsPath} component={LoadingContacts} /> {/* this is more a loading screen doenst need on route but only for making the page*/}
-
       </Route>
     </Router>
   </Provider>,
   document.getElementById('root')
-)
+);
