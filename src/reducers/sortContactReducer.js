c7ab4aa89f972481fc176e477404b13eae576c0e
raptorzhezuzninya <w22243vrh@gmail.com>
2017-06-11 22:01:51 +0200

    renamed undoable to prevNextAble

--- src/reducers/sortContactReducer.js
+++ src/reducers/sortContactReducer.js
@@ -10,20 +10,17 @@ const initialState = {
 const sortContactReducer = (state = initialState, { type, payload } = {}) => {
   switch (type) {
     case RECEIVE_CONTACTS:
-      console.log(payload, 'payload RECEIVE_CONTACTS');
       // add property isDeleted: false to each contact i get passed in
       const enhancedPayload = payload.map(contact => {
         return { ...contact, isDeleted: false };
       });
-      console.log(enhancedPayload, 'enhancedPayload');
       return { ...state, totalSortContacts: enhancedPayload };
 
     default:
-      console.log(state, 'STATE in sortContactReducer');
       return state;
   }
 };
 
-const undoableSortContactReducer = undoable(sortContactReducer);
+const prevNextAbleSortContactReducer = prevNextAble(sortContactReducer);
 
-export default undoableSortContactReducer;
+export default prevNextAbleSortContactReducer;
