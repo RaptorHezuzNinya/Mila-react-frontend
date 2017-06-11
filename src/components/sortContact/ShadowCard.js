b663e9e3a06f4cc954f00a43d8fc3395d70d2679
raptorzhezuzninya <w22243vrh@gmail.com>
2017-06-11 17:40:30 +0200

    add default text if value is emprty

--- src/components/sortContact/ShadowCard.js
+++ src/components/sortContact/ShadowCard.js
@@ -14,10 +14,19 @@ class ShadowCard extends PureComponent {
       <Paper zDepth={2} className="contact-card-paper">
         <div className="details-holder">
           <div className="details">
-            <p className="par1">{contact.firstName}</p>
-            <p className="par2">{contact.lastName}</p>
-            <p className="par3">{contact.companyName}</p>
-            <p className="par4">{contact.companyRole}</p>
+            <p className="par1">
+              {!contact.firstName ? 'First Name' : contact.firstName}
+            </p>
+            <p className="dot">•</p>
+            <p className="par2">
+              {!contact.lastName ? 'last name' : contact.lastName}
+            </p>
+            <p className="par3">
+              {!contact.companyName ? 'Company' : contact.companyName}
+            </p>
+            <p className="par4">
+              {!contact.companyRole ? 'Role' : contact.companyRole}
+            </p>
           </div>
           <div className="avatar-holder">
             <img src={contact.avatar} alt="user avatar" />
