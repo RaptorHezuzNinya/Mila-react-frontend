146d2af2f08c1afaf03a7b6a22b594e7d189f3ba
raptorzhezuzninya <w22243vrh@gmail.com>
2017-06-15 23:31:52 +0200

    refactor func to class comp for the logic i need

--- src/components/sortContact/NavigateContacts.js
+++ src/components/sortContact/NavigateContacts.js
@@ -1,54 +1,60 @@
-import React, { PropTypes } from 'react';
+import React, { PureComponent } from 'react';
+import { PropTypes } from 'prop-types';
 import IconButton from 'material-ui/IconButton';
 import './NavigateContacts.sass';
 
-const hoverOutDiv = () => {
-  let addedClassElement = document.querySelector('.custom-hover');
-  addedClassElement.classList.remove('custom-hover');
-};
+class NavigateContacts extends PureComponent {
+  static propTypes = {
+    children: PropTypes.object.isRequired,
+    handlePrevContact: PropTypes.func.isRequired,
+    handleNextContact: PropTypes.func.isRequired,
+  };
 
-const NavigateContacts = ({
-  children,
-  handleNextContact,
-  handlePrevContact,
-}) => {
-  return (
-    <div className="navigate-contacts-holder">
-      <div
-        className="contact-navigate-prev move-left"
-        onMouseEnter={() => {
-          return document
-            .getElementById('past-shadow')
-            .classList.add('custom-hover');
-        }}
-        onMouseOut={hoverOutDiv}
-      >
-        <IconButton onClick={handlePrevContact} iconClassName="material-icons">
-          navigate_before
-        </IconButton>
-      </div>
-      {children}
-      <div
-        className="contact-navigate-next move-right"
-        onMouseEnter={() => {
-          return document
-            .getElementById('fut-shadow')
-            .classList.add('custom-hover');
-        }}
-        onMouseOut={hoverOutDiv}
-      >
-        <IconButton onClick={handleNextContact} iconClassName="material-icons">
-          navigate_next
-        </IconButton>
-      </div>
-    </div>
-  );
-};
+  hoverOutDiv() {
+    let addedClassElement = document.querySelector('.custom-hover');
+    addedClassElement.classList.remove('custom-hover');
+  }
 
-NavigateContacts.propTypes = {
-  children: PropTypes.object.isRequired,
-  handlePrevContact: PropTypes.func.isRequired,
-  handleNextContact: PropTypes.func.isRequired,
-};
+  render() {
+    const { handleNextContact, handlePrevContact, children } = this.props;
+    return (
+      <div className="navigate-contacts-holder">
+        <div
+          className="contact-navigate-prev move-left"
+          onMouseEnter={() => {
+            return document
+              .getElementById('past-shadow')
+              .classList.add('custom-hover');
+          }}
+          onMouseOut={this.hoverOutDiv}
+        >
+          <IconButton
+            onClick={handlePrevContact}
+            iconClassName="material-icons"
+          >
+            navigate_before
+          </IconButton>
+        </div>
+        {children}
+        <div
+          className="contact-navigate-next move-right"
+          onMouseEnter={() => {
+            return document
+              .getElementById('fut-shadow')
+              .classList.add('custom-hover');
+          }}
+          onMouseOut={this.hoverOutDiv}
+        >
+          <IconButton
+            onClick={handleNextContact}
+            iconClassName="material-icons"
+          >
+            navigate_next
+          </IconButton>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default NavigateContacts;
