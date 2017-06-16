7d3cd2760edd7f6ee49cc4e104c358e2ae090ed4
raptorzhezuzninya <w22243vrh@gmail.com>
2017-06-16 18:24:45 +0200

    removed donesorting comp own stylesheet

--- src/components/sortContact/DoneSorting.js
+++ src/components/sortContact/DoneSorting.js
@@ -2,7 +2,6 @@ import React, { PureComponent } from 'react'
 import PageTitle from '../PageTitle'
 import FlatButton from 'material-ui/FlatButton'
 import GroupAdd from 'material-ui/svg-icons/social/group-add'
-import './DoneSorting.sass'
 
 const DoneSorting = ({ deletedSortContacts, past }) => {
   return (
@@ -12,19 +11,23 @@ const DoneSorting = ({ deletedSortContacts, past }) => {
         pageTitleContentH2="Well done!"
         pageTitleContentH3="All sorted for now - feels good huh?"
       />
-      <div className="done-card">
-        <div className="icon-number">
-          <GroupAdd className="icon" color="#f2f6fa" />
-          <p className="number">{past.length - deletedSortContacts.length}</p>
+      <div className="done-card-wrapper">
+        <div className="done-card">
+          <div className="icon-number">
+            <GroupAdd className="icon" color="#f2f6fa" />
+            <p className="number">{past.length - deletedSortContacts.length}</p>
+          </div>
+          <p className="done-text">Contacts added to your network</p>
         </div>
-        <p className="done-text">Contacts added to your network</p>
       </div>
+      {/* <div className="wrapper-btn"> */}
       <div className="button-holder">
         <FlatButton className="btn-l-a my-network" label="to my network" />
         <FlatButton className="btn-l-a" label="sort more contacts" />
       </div>
     </div>
   )
+  // </div>
 }
 
 export default DoneSorting
