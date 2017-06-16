3aa1573b6885ca328b9023693ba5916f94176897
raptorzhezuzninya <w22243vrh@gmail.com>
2017-06-16 17:00:52 +0200

    renamed totalcontacts in state to totalContCount because i have a prop named totalcontacts in render i check is donesorting is true

--- src/components/sortContact/SortContactContainer.js
+++ src/components/sortContact/SortContactContainer.js
@@ -1,30 +1,27 @@
-import React, { PureComponent } from 'react';
-import { PropTypes } from 'prop-types';
-import { connect } from 'react-redux';
-import { submit } from 'redux-form';
-import { updateContact } from '../../actions/contacts';
-import {
-  addContactToDeleted,
-  undoAddContactToDeleted,
-} from '../../actions/sortContacts';
-import { fetchContacts } from '../../actions/sortContacts';
-import { prev, next } from '../../actions/prevNextAble';
-import { formFieldsContactDetails as formFields } from '../../helpers/formData';
-import _ from 'lodash';
-import Media from 'react-media';
-import ContactCard from './ContactCard';
-import DoneSorting from './DoneSorting';
-import DeletedContactCard from './DeletedContactCard';
-import DummyCard from './DummyCard';
-import ProgressIndicator from '../ProgressIndicator';
-import NetworkListButton from './NetworkListButton';
-import PageTitle from '../PageTitle';
-import NavigateContacts from './NavigateContacts';
-import HintFooter from './HintFooter';
-import Snackbar from 'material-ui/Snackbar';
-import FlatButton from 'material-ui/FlatButton';
-import NotIcon from 'material-ui/svg-icons/av/not-interested';
-import './SortContactContainer.sass';
+import React, { PureComponent } from 'react'
+import { PropTypes } from 'prop-types'
+import { connect } from 'react-redux'
+import { submit } from 'redux-form'
+import { updateContact } from '../../actions/contacts'
+import { addContactToDeleted, undoAddContactToDeleted } from '../../actions/sortContacts'
+import { fetchContacts } from '../../actions/sortContacts'
+import { prev, next } from '../../actions/prevNextAble'
+import { formFieldsContactDetails as formFields } from '../../helpers/formData'
+import _ from 'lodash'
+import Media from 'react-media'
+import ContactCard from './ContactCard'
+import DoneSorting from './DoneSorting'
+import DeletedContactCard from './DeletedContactCard'
+import DummyCard from './DummyCard'
+import ProgressIndicator from '../ProgressIndicator'
+import NetworkListButton from './NetworkListButton'
+import PageTitle from '../PageTitle'
+import NavigateContacts from './NavigateContacts'
+import HintFooter from './HintFooter'
+import Snackbar from 'material-ui/Snackbar'
+import FlatButton from 'material-ui/FlatButton'
+import NotIcon from 'material-ui/svg-icons/av/not-interested'
+import './SortContactContainer.sass'
 
 class SortContactContainer extends PureComponent {
   static propTypes = {
@@ -36,163 +33,143 @@ class SortContactContainer extends PureComponent {
     contactDetailsForm: PropTypes.object,
     next: PropTypes.func.isRequired,
     prev: PropTypes.func.isRequired,
-    addedContactIds: PropTypes.array.isRequired,
-  };
+    addedContactIds: PropTypes.array.isRequired
+  }
 
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       snackProceed: false,
-      // snackDelete: false,
       contactIndex: 0,
       curContactNumb: 1,
-      totalContacts: null,
-      completedProgress: 4,
-    };
-    this.handleNextContact = this.handleNextContact.bind(this);
-    this.handlePrevContact = this.handlePrevContact.bind(this);
-    this.handleDeleteContact = this.handleDeleteContact.bind(this);
+      totalContCount: null,
+      completedProgress: 4
+    }
+    this.handleNextContact = this.handleNextContact.bind(this)
+    this.handlePrevContact = this.handlePrevContact.bind(this)
+    this.handleDeleteContact = this.handleDeleteContact.bind(this)
     // this.handleContainerKeyPress = this.handleContainerKeyPress.bind(this);
   }
 
   componentDidMount() {
-    this.props.fetchContacts();
-    window.addEventListener('keydown', this.handleContainerKeyPress);
+    this.props.fetchContacts()
+    window.addEventListener('keydown', this.handleContainerKeyPress)
   }
 
   componentWillReceiveProps() {
-    console.log('when??');
+    const { totalContacts } = this.props
+    console.log('when??')
     this.setState({
-      totalContacts: this.props.totalContacts.length,
-    });
+      totalContCount: totalContacts.length
+    })
   }
 
   componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleContainerKeyPress);
+    window.removeEventListener('keydown', this.handleContainerKeyPress)
   }
 
   handleRequestClose = () => {
     this.setState({
-      snackProceed: false,
-    });
-  };
+      snackProceed: false
+    })
+  }
 
   onSubmit = (values, dispatch, props) => {
-    const { currentContact } = this.props;
-    const initVal = props.initialValues;
-    // console.log('val before', values)
+    const { currentContact } = this.props
+    const initVal = props.initialValues
     formFields.forEach(field => {
       if (!values[field]) {
-        values[field] = initVal[field];
+        values[field] = initVal[field]
       }
-      return values;
-    });
+      return values
+    })
     if (props.dirty) {
-      return dispatch(updateContact(values, currentContact));
+      return dispatch(updateContact(values, currentContact))
     }
-  };
+  }
 
   handleDeleteContact() {
-    const { currentContact } = this.props;
-    this.props.addContactToDeleted(currentContact);
+    const { currentContact } = this.props
+    this.props.addContactToDeleted(currentContact)
   }
 
   handleContainerKeyPress = event => {
-    const { contactDetailsForm } = this.props;
+    const { contactDetailsForm } = this.props
     if (!contactDetailsForm) {
       if (event.keyCode === 37) {
-        return this.handlePrevContact();
+        return this.handlePrevContact()
       } else if (event.keyCode === 39) {
-        return this.handleNextContact();
+        return this.handleNextContact()
       } else {
-        return null;
+        return null
       }
     } else {
-      let arr = [];
-      let activeField;
+      let arr = []
+      let activeField
       formFields.forEach(field => {
-        activeField = _.get(
-          contactDetailsForm.fields,
-          [field, 'active'],
-          false
-        );
-        return arr.push(activeField);
-      });
+        activeField = _.get(contactDetailsForm.fields, [field, 'active'], false)
+        return arr.push(activeField)
+      })
       if (arr.includes(true)) {
-        return;
+        return
       } else if (event.keyCode === 37) {
-        return this.handlePrevContact();
+        return this.handlePrevContact()
       } else if (event.keyCode === 39) {
-        return this.handleNextContact();
+        return this.handleNextContact()
       } else if (event.keyCode === 88) {
-        return this.handleDeleteContact();
+        return this.handleDeleteContact()
       }
     }
-  };
+  }
 
   handleRemoteContactDetailSubmit() {
-    return this.props.submit('contactDetailsForm');
+    return this.props.submit('contactDetailsForm')
   }
 
   async handleNextContact() {
-    const {
-      contactIndex,
-      curContactNumb,
-      totalContacts,
-      completedProgress,
-    } = this.state;
-    const { addedContactIds, currentContact, next } = this.props;
-    await this.handleRemoteContactDetailSubmit();
-    if (contactIndex >= totalContacts - 1) return null;
+    const { contactIndex, curContactNumb, totalContCount, completedProgress } = this.state
+    const { addedContactIds, currentContact, next } = this.props
+    await this.handleRemoteContactDetailSubmit()
+    // if (contactIndex >= totalContCount - 1) return null
 
-    if (
-      addedContactIds.includes(currentContact.id) ||
-      currentContact.isDeleted
-    ) {
+    if (addedContactIds.includes(currentContact.id) || currentContact.isDeleted) {
       this.setState({
-        completedProgress: completedProgress + 100 / totalContacts,
+        completedProgress: completedProgress + 100 / totalContCount,
         contactIndex: contactIndex + 1,
-        curContactNumb: curContactNumb + 1,
-      });
-      return next();
+        curContactNumb: curContactNumb + 1
+      })
+      return next()
     } else {
       return this.setState({
-        snackProceed: true,
-      });
+        snackProceed: true
+      })
     }
   }
 
   async handlePrevContact() {
-    const {
-      contactIndex,
-      curContactNumb,
-      totalContacts,
-      completedProgress,
-    } = this.state;
-    await this.handleRemoteContactDetailSubmit();
-    if (contactIndex === 0) return null;
+    const { contactIndex, curContactNumb, totalContCount, completedProgress } = this.state
+    await this.handleRemoteContactDetailSubmit()
+    if (contactIndex === 0) return null
     this.setState({
       contactIndex: contactIndex - 1,
       curContactNumb: curContactNumb - 1,
-      completedProgress: completedProgress - 100 / totalContacts,
-    });
-    return this.props.prev();
+      completedProgress: completedProgress - 100 / totalContCount
+    })
+    return this.props.prev()
   }
 
-  handleUndo = () => {
-    console.log('Undo works with second snackbar');
-    return this.props.undoAddContactToDeleted(this.props.currentContact);
-  };
+  handleUndo = () => this.props.undoAddContactToDeleted(this.props.currentContact)
 
   render() {
+    const { curContactNumb, totalContCount, completedProgress, snackProceed } = this.state
     const {
-      curContactNumb,
-      totalContacts,
-      completedProgress,
-      snackProceed,
-    } = this.state;
+      currentContact,
+      futureContact,
+      pastContact,
+      doneSorting,
+      deletedSortContacts
+    } = this.props
 
-    const { currentContact, futureContact, pastContact, future } = this.props;
     let whichCard = !currentContact.isDeleted
       ? <div className="contact-card-wrapper">
           <ContactCard onSubmit={this.onSubmit} />
@@ -202,26 +179,20 @@ class SortContactContainer extends PureComponent {
             contact={currentContact}
             deleteCopy="will be kept out of my records."
           />
-        </div>;
+        </div>
 
-    const lastPastContact = pastContact[pastContact.length - 1];
-    if (true) {
-      return <DoneSorting />;
+    const lastPastContact = pastContact[pastContact.length - 1]
+
+    if (doneSorting) {
+      return <DoneSorting past={pastContact} deletedSortContacts={deletedSortContacts} />
     } else {
       return (
         <div className="global-wrapper">
           <Media
             query="(min-width: 1280px)"
             render={() => (
-              <div
-                className="past-wrapper"
-                id="past-shadow"
-                onClick={this.handlePrevContact}
-              >
-                <DummyCard
-                  contact={lastPastContact}
-                  paperClass="dummy-paper-prev"
-                />
+              <div className="past-wrapper" id="past-shadow" onClick={this.handlePrevContact}>
+                <DummyCard contact={lastPastContact} paperClass="dummy-paper-prev" />
               </div>
             )}
           />
