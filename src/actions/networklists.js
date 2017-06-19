16bfd9f6bf19e8419220b634d9e2b3625d0f0dfa
raptorzhezuzninya <w22243vrh@gmail.com>
2017-06-19 19:54:38 +0200

    add rm_contact from networklist action

--- src/actions/networklists.js
+++ src/actions/networklists.js
@@ -1,38 +1,49 @@
-export const CREATE_NETWORKLIST = 'CREATE_NETWORKLIST';
-let nextNWLId = 3;
+export const CREATE_NETWORKLIST = 'CREATE_NETWORKLIST'
+let nextNWLId = 3
 export const createNetworkList = networkList => {
   return {
     type: CREATE_NETWORKLIST,
     payload: {
       networkList,
-      id: nextNWLId++,
-    },
-  };
-};
+      id: nextNWLId++
+    }
+  }
+}
 
-export const UPDATE_NETWORKLIST = 'UPDATE_NETWORKLIST';
+export const UPDATE_NETWORKLIST = 'UPDATE_NETWORKLIST'
 export const updateNetworkList = networkList => {
   return {
     type: UPDATE_NETWORKLIST,
-    payload: networkList,
-  };
-};
+    payload: networkList
+  }
+}
 
-export const DELETE_NETWORKLIST = 'DELETE_NETWORKLIST';
+export const DELETE_NETWORKLIST = 'DELETE_NETWORKLIST'
 export const deleteNetworkList = id => {
   return {
     type: DELETE_NETWORKLIST,
-    payload: id,
-  };
-};
+    payload: id
+  }
+}
 
-export const ADD_CONTACT_TO_NETWORKLIST = 'ADD_CONTACT_TO_NETWORKLIST';
-export const addContactToNetworklist = (contactId, networkListId) => {
+export const ADD_CONTACT_TO_NETWORKLIST = 'ADD_CONTACT_TO_NETWORKLIST'
+export const addContactToNetworkList = (contact, networkList) => {
   return {
     type: ADD_CONTACT_TO_NETWORKLIST,
     payload: {
-      networkListId,
-      contactId,
-    },
-  };
-};
+      networkList,
+      contact
+    }
+  }
+}
+
+export const RM_CONTACT_FROM_NETWORKLIST = 'RM_CONTACT_FROM_NETWORKLIST'
+export const rmContactFromNetworkList = (contact, networkList) => {
+  return {
+    type: RM_CONTACT_FROM_NETWORKLIST,
+    payload: {
+      contact,
+      networkList
+    }
+  }
+}
